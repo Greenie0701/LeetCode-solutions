cf76f3491a7e8316d7c32e653912d25df892e2f1
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-05 19:10:17 +0530

    1482-minimum-number-of-days-to-make-m-bouquets

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,55 +1,49 @@
 class Solution {
 public:
-    int possible(vector<int>& bloomDay, int day, int m, int k){
-        int pos = 0;
+    bool possible(vector<int>& bloomDay, int day, int m, int k){
         int count = 0;
+        int bqcount = 0;
         for(int i=0; i<bloomDay.size(); i++){
+            // Check if the flower is bloomed within day
             if(bloomDay[i]<=day){
+                // Increase the count, since the flower is bloomed
                 count++;
+                // If count is reached to k flowers, then yes one boq made
                 if(count==k){
-                    pos+=1;
-                    count=0;
+                    bqcount++;
+                    count =0;
                 }
-            }else{
-                count=0;
+            }
+            // Else restart the count
+            else{
+                count = 0;
             }
         }
-        if(pos>=m){
-            return 1;
+        if(bqcount>=m){
+            return true;
         }else{
-            return -1;
+            return false;
         }
-    }
-    int max_day(vector<int>& bloomDay, int size){
-        int maxi = INT_MIN;
-        for(int i=0; i<size; i++){
-            maxi = max(bloomDay[i], maxi);
-        }
-        return maxi;
-    }
-    int min_day(vector<int>& bloomDay, int size){
-        int mini = INT_MAX;
-        for(int i=0; i<size; i++){
-            mini = min(bloomDay[i], mini);
-        }
-        return mini;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        // Handle the edge case, where you have less no of flowers to make boques
-        if(m*k>bloomDay.size())return -1;
-        // Perform binary search
-        int mini = INT_MAX;
-        int low = min_day(bloomDay, bloomDay.size());
-        int high = max_day(bloomDay, bloomDay.size());
+        // Handle the edge case where there is no enough flowers to make bouquet
+        long long val = (long long)m*(long long)k;
+        if(val>bloomDay.size()){
+            return -1;
+        }
+        // Low = Minimum day taken to bloom one flower
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        // High = Maximum day taken to bloom all flowers
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+        // Iterate through the range to find the min days to make k no of bouquets
         while(low<=high){
-            int mid = low + (high-low)/2;
-            if(possible(bloomDay, mid, m, k)==1){
-                mini = min(mini, mid);
+            int mid = low+(high-low)/2;
+            if(possible(bloomDay, mid, m, k)){
                 high = mid-1;
             }else{
                 low = mid+1;
             }
         }
-        return mini;
+        return low;
     }
 };
