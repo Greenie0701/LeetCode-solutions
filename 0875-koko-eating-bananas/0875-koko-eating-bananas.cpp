83c0e07fa23522b434aa4c59b8e36623d657327c
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-05 18:47:38 +0530

    0875-koko-eating-bananas

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-    int findMax(vector<int> &v) {
-        int maxi = INT_MIN;
-        int n = v.size();
-        //find the maximum:
-        for (int i = 0; i < n; i++) {
-            maxi = max(maxi, v[i]);
+    bool possible(vector<int>& piles, int size, int speed, int h){
+        long long k = 0;
+        for(int i=0; i<size; i++){
+            k+=ceil(double(piles[i])/double(speed));
         }
-        return maxi;
+        if(k<=h){
+            return true;
+        }else{
+            return false;
+        }
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int min_speed = INT_MAX;
+        // Initialise the range for the search
         int low = 1;
-        int high = findMax(piles);
+        int high = *max_element(piles.begin(), piles.end());
+        int size = piles.size();
+        int min_speed = INT_MAX;
+        // Search through the entire range for minimal speed
         while(low<=high){
-            // find the mid of k 
-            int mid = low + (high - low)/2;
-            long long k = 0;
-            // Calculate the total hours taken to complete the total piles with mid
-            for(int i = 0; i<=piles.size()-1; i++){
-                k += ceil(double(piles[i])/double(mid));
-            }
-            // Check if the calculated k hours is less than or eq given hours
-            if(k<=h){
-                min_speed = min(min_speed, mid);
-                high = mid-1;
-            }
-            // Calculated K is not enough so search from right
-            else{
-                low=mid+1;
+            int mid = low+(high-low)/2;
+            if(possible(piles, size, mid, h)){
+                min_speed=min(mid, min_speed);
+                high =  mid-1;
+            }else{
+                low = mid+1;
             }
         }
-        return min_speed;
+        return low;
     }
 };
