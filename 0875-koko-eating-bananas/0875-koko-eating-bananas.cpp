f595b49d39167ca6517fc2031e445d458318e348
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-08 19:03:23 +0530

    0875-koko-eating-bananas

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    bool possible(vector<int>& piles, int size, int speed, int h){
-        long long k = 0;
-        for(int i=0; i<size; i++){
-            k+=ceil(double(piles[i])/double(speed));
+    bool possible(vector<int>& piles, int k, int h){
+        int size = piles.size();
+        long long hours = 0;
+        for(int i = 0; i<size; i++){
+            hours+=ceil(double(piles[i])/double(k));
         }
-        if(k<=h){
+        if(hours<=h){
             return true;
-        }else{
+        }
+        else{
             return false;
         }
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        // Initialise the range for the search
+        // Initialise the search space
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
-        int size = piles.size();
-        int min_speed = INT_MAX;
-        // Search through the entire range for minimal speed
+        int ans = -1;
+        // Perform binary search to 
         while(low<=high){
             int mid = low+(high-low)/2;
-            if(possible(piles, size, mid, h)){
-                min_speed=min(mid, min_speed);
-                high =  mid-1;
-            }else{
+            if(possible(piles, mid, h)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
                 low = mid+1;
             }
         }
-        return low;
+        return ans;
     }
 };
