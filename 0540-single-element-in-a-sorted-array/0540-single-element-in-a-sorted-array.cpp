f91b75c16c2532cd8e3f24a60369fd7fc09c53c0
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-08 18:11:39 +0530

    0540-single-element-in-a-sorted-array

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // Handle the edge cases 
-        int n = nums.size();
-        if(n==1){
+        int low = 0;
+        int high = nums.size()-1;
+        if(nums.size()==1){
             return nums[0];
         }
-        // My starting element is unique
-        if(nums[0]!=nums[1]){
-            return nums[0];
+        if(nums[low]!=nums[1]){
+            return nums[low];
         }
-        // Check the last two elements(Avoids out of bounds)
-        if(nums[n-2]!=nums[n-1]){
-            return nums[n-1];
+        if(nums[high]!=nums[high-1]){
+            return nums[high];
         }
-
-        int low = 1;
-        int high = n-2;
+        low = 1;
+        high = high-1;
         while(low<=high){
-            int mid = low + (high - low)/2;
-            // Base case
-            if((nums[mid]!=nums[mid+1])&&(nums[mid]!=nums[mid-1])){
+            int mid =  low+(high-low)/2;
+            if((nums[mid-1]!=nums[mid])&&(nums[mid+1]!=nums[mid])){
                 return nums[mid];
             }
-            // Check if mid is even, if yes -> my next neighbour should equal
-            // or if mid is odd, if yes -> my prev neighbour should equal
-            // if any one of the case is true, then my unique should present in the right half
-            else if((mid % 2 ==0 && nums[mid]==nums[mid+1])|| (mid % 2 ==1 && nums[mid]==nums[mid-1])){
-                low = mid+1;
-            }
-            // Else my unique is present in left
-            else{
-                high = mid -1;
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+                (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
