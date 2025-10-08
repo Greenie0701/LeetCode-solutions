2465bdf8b503fc574f3690ca1547a19c6b21c53e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-08 17:18:54 +0530

    0081-search-in-rotated-sorted-array-ii

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -3,36 +3,31 @@ public:
     bool search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size()-1;
-        int k = target;
         while(low<=high){
-            // Calculate the mid value, make sure not to occur any overflow
             int mid = low + (high-low)/2;
-            // Check if the mid is actually the target
             if(nums[mid]==target){
                 return true;
             }
-            // Check if any duplicates exist, if yes skip
-            if (nums[low] == nums[mid] && nums[mid] == nums[high]){
-                low = low+1;
-                high = high-1;
-                continue;
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
+                low++;
+                high--;
             }
-            // Check if the left half is sorted, then search in left half
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=k && k<nums[mid]){
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
                     high = mid-1;
-                }else{
+                }
+                else{
                     low = mid+1;
                 }
             }
-            // Check if the right half is sorted, then search in right half
-            else if(nums[mid]<=nums[high]){
-                if(k>nums[mid] && k<=nums[high]){
+            else{
+                if(target<=nums[high] && target>nums[mid]){
                     low = mid+1;
-                }else{
+                }
+                else{
                     high = mid-1;
                 }
-            }
+            } 
         }
         return false;
     }
