6f7480716ccc27f727fa2c08d21fe8edabae1f97
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-08 07:25:17 +0530

    0240-search-a-2d-matrix-ii

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    bool searchTarget(vector<int>& matrix, int target){
-        // Simply perform the binary search in the 1-D array
-        int low = 0;
-        int high = matrix.size()-1;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(matrix[mid]==target){
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // Assign the row and column values for traversal
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int row = 0;
+        int col = m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target){
                 return true;
             }
-            else if(matrix[mid]<target){
-                low = mid+1;
+            // Check if the elements in that row is smaller than target, if yes target may not be found
+            // This is because all row are in decreasing and col wise increasing
+            else if(matrix[row][col]<target){
+                row++;
             }
             else{
-                high = mid-1;
-            }
-        }
-        return false;
-    }
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0; i<matrix.size(); i++){
-            if(searchTarget(matrix[i],target)){
-                return true;
-            }else{
-                continue;
+                col--;
             }
         }
         return false;
