a64c62c1438df9f5619841dd48caed0ad2c4a2f8
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-09 08:12:26 +0530

    1011-capacity-to-ship-packages-within-d-days

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool possible(vector<int>& weights, int k,  int days){
+        int d =1;
+        int current = 0;
+        for(int i=0; i<weights.size(); i++){
+            if (weights[i] > k) return false;
+            if(current+weights[i]>k){
+                d++;
+                current=weights[i];
+            }else{
+                current+=weights[i];
+            }
+        }
+        if(d<=days){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        // Find the range of capacity. It can hold either min element of capacity or max weight
+        int low = *min_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        int ans = high;
+        // Find the minimum capacity where all weights can be shipped
+        while(low<=high){
+            int mid = low+(high - low)/2;
+            if(possible(weights, mid, days)){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
