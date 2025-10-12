d7b32d6c87b29406a05da5e773913a724c5b1d00
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-12 08:15:25 -0700

    0240-search-a-2d-matrix-ii

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // Assign the row and column values for traversal
-        int n = matrix.size();
         int m = matrix[0].size();
+        int n = matrix.size();
         int row = 0;
         int col = m-1;
         while(row<n && col>=0){
             if(matrix[row][col]==target){
                 return true;
             }
-            // Check if the elements in that row is smaller than target, if yes target may not be found
-            // This is because all row are in decreasing and col wise increasing
             else if(matrix[row][col]<target){
                 row++;
             }
