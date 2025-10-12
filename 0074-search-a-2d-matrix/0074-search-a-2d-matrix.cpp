e609ce1b132ce4096927bc968eda3a6727187d0b
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-12 07:23:36 -0700

    0074-search-a-2d-matrix

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // Initialise low and high value
-        int n = matrix.size();
         int m = matrix[0].size();
-        // We can assume that we flatten the array and initialise the low and high index
+        int n = matrix.size();
         int low = 0;
-        int high = m+n-1;
+        int high = m*n-1;
+        // Perform the binary search to obtain the value
         while(low<=high){
-            int mid = low+(high-low)/2;
-            // Get the mid index in 2D matrix
-            int row = mid/m;
+            int mid = low + (high-low)/2;
             int col = mid%m;
-            // Now cross check with the mid value with target
+            int row = mid/m;
             if(matrix[row][col]==target){
                 return true;
-            }else if(matrix[row][col]<target){
+            }
+            else if(matrix[row][col]<target){
                 low = mid+1;
-            }else{
+            }
+            else{
                 high = mid-1;
             }
         }
