ad49cbc5fbbd49716d51b34fce2e803802f31b7d
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-12 08:49:01 -0700

    0004-median-of-two-sorted-arrays

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -11,13 +11,13 @@ public:
         int half = (total + 1) / 2;
 
         while (low <= high) {
-            int m1 = (low + high) / 2;
-            int m2 = half - m1;
+            int mid1 = (low + high) / 2;
+            int mid2 = half - mid1;
 
-            int l1 = (m1 > 0) ? nums1[m1 - 1] : INT_MIN;
-            int l2 = (m2 > 0) ? nums2[m2 - 1] : INT_MIN;
-            int r1 = (m1 < n1) ? nums1[m1] : INT_MAX;
-            int r2 = (m2 < n2) ? nums2[m2] : INT_MAX;
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
+            int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
 
             if (l1 <= r2 && l2 <= r1) {
                 if (total % 2 == 0)
@@ -25,9 +25,9 @@ public:
                 else
                     return double(max(l1, l2));
             } else if (l1 > r2) {
-                high = m1 - 1;
+                high = mid1 - 1;
             } else {
-                low = m1 + 1;
+                low = mid1 + 1;
             }
         }
         return 0.0;
