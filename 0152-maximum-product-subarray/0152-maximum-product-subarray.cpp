700d32a349b41138b0eb4a4cf69e8c4b3bfff94e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-13 07:02:09 -0700

    0152-maximum-product-subarray

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // Initialise max value to INT_MIN
-        int maxproduct = INT_MIN;
-        // Initialise prefix and suffix to 1
+        if(nums.size()==1){
+            return nums[0];
+        }
+        int size = nums.size();
         int prefix = 1;
         int suffix = 1;
-        // Lets calculate the prefix and suffix product
-        for(int i=0;i<nums.size();i++){
-            // If my prefix/suffix becomes zero, just reset to 1
+        int maxi = INT_MIN;
+        for(int i=0;i<size;i++){
             if(prefix==0)prefix=1;
             if(suffix==0)suffix=1;
-            //Calculate product from the prefix and suffix end
-            prefix=prefix * nums[i];
-            suffix=suffix * nums[nums.size()-i-1];
-            // Update the maximum product
-            maxproduct= max(maxproduct,max(prefix, suffix));
+            prefix = prefix*nums[i];
+            suffix = suffix*nums[size-1-i];
+            maxi = max(maxi, max(prefix, suffix));
         }
-        return maxproduct;
+        return maxi;
     }
 };
