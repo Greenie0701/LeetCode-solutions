0ec10f6c990e83dd28327c831b977dbfd4809ebd
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-13 08:09:27 -0700

    0001-two-sum

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,23 +2,26 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<pair<int,int>> arr;
-        for(int i=0; i<nums.size(); i++) {
-            arr.push_back({nums[i], i});
+        int index = 0;
+        for(int i:nums){
+            arr.push_back({i, index});
+            index++;
         }
-
-        sort(arr.begin(), arr.end());  // sort by value
-
-        int left = 0, right = arr.size()-1;
-        while(left < right) {
-            int sum = arr[left].first + arr[right].first;
-            if(sum < target) {
+        sort(arr.begin(), arr.end());
+        int left = 0;
+        int right =  nums.size()-1;
+        while(left<right){
+            int sum = arr[left].first+arr[right].first;
+            if(sum<target){
                 left++;
-            } else if(sum > target) {
+            }
+            else if(sum>target){
                 right--;
-            } else {
-                return {arr[left].second, arr[right].second};
+            }
+            else{
+                return {arr[left].second,arr[right].second};
             }
         }
-        return {}; // should not reach here
+        return {};
     }
 };
