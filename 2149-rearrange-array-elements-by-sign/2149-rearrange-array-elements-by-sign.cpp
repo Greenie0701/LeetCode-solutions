a2832f1b8f7f226af36c932baedebe5097d9a74d
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-13 19:28:39 -0700

    2149-rearrange-array-elements-by-sign

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> ans{size,0};
+        int neg = 1;
+        int pos = 0; 
+        for(int i=0;i<size;i++){
+            if(nums[i]<0){
+                ans[neg] = nums[i];
+                neg+=2;
+            }
+            else{
+                ans[pos]=nums[i];
+                pos+=2;
+            }
+        }
+        return ans;
+    }
+};
