2d0856f281385707b3ed08a7071056b3e79b10b0
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-13 19:06:35 -0700

    0053-maximum-subarray

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // Kadane's Algorithm is a popular method to solve the Maximum Subarray Sum Problem,   
         int sum = 0;
         int maxsum = INT_MIN;
-        
-        // Iterate through the array 
-        for(int i=0; i<nums.size(); i++){
+        for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            maxsum=max(sum, maxsum);
             if(sum<0){
-                sum = 0;
+                maxsum = max(sum, maxsum);
+                sum=0;
+            }
+            else{
+                sum+=nums[i];
+                maxsum=max(sum, maxsum);
             }
         }
         return maxsum;
