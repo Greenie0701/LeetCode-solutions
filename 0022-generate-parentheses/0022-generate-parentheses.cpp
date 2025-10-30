20fd361792efb17835c3653ed9fe77010a788f15
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-30 06:24:15 -0700

    0022-generate-parentheses

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void genseq(vector<string>& ans, int open, int close, string s, int n){
-        if(s.length()==2*n){
-            ans.push_back(s);
+    void ParenthesesCombination(vector<string>& result, int open, int close, int size, string valid){
+        if(valid.length()==size*2){
+            result.push_back(valid);
             return;
         }
-        if(open<n)genseq(ans, open+1, close, s+"(", n);
-        if(close<open)genseq(ans, open, close+1, s+")", n);
+        if(open<size)ParenthesesCombination(result, open+1, close, size, valid+"(");
+        if(close<open)ParenthesesCombination(result, open, close+1, size, valid+")");
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> ans;
-        genseq(ans, 0, 0,"", n);
-        return ans;
+        vector<string> result;
+        ParenthesesCombination(result, 0, 0, n, "");
+        return result;
     }
 };
