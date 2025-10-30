cf07fd7a897f4249db9648f322c79fd571622b1f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-30 06:47:18 -0700

    0078-subsets

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    void findsubset(vector<int>& nums, vector<int>& curr, vector<vector<int>>& ans,int index,int size){
-        if(index>size){
-            if(curr.size()==0){
-                ans.push_back({});
-                return;
-            }
-            ans.push_back(curr);
+    void generate(vector<int>& nums, vector<vector<int>>& powerset, vector<int>& subset, int index){
+        if(index==nums.size()){
+            powerset.push_back(subset);
             return;
         }
-        curr.push_back(nums[index]);
-        findsubset(nums, curr, ans, index+1, size);
-        curr.pop_back();
-        findsubset(nums, curr, ans, index+1, size);
+        generate(nums, powerset, subset, index+1);
+        subset.push_back(nums[index]);
+        generate(nums, powerset, subset, index+1);
+        subset.pop_back();
+        
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-        findsubset(nums,curr, ans, 0, nums.size()-1);
-        return ans;
+        vector<vector<int>> powerset;
+        vector<int> subset;
+        generate(nums, powerset, subset, 0);
+        return powerset;
     }
 };
