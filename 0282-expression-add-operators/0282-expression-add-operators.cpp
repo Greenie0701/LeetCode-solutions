6afe13d0c06e9e4fdc01c8bbcc7a0b2c222cde59
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-10-29 19:42:59 -0700

    0282-expression-add-operators

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    void solve(string num, int target, int index, int curr, int prev, string exp, vector<string>& result) {
-        if(index==num.length()){
+    void createexp(string num, int target, vector<string>& result, int index, long curr, long prev, string exp){
+        if(index==num.size()){
             if(curr==target){
                 result.push_back(exp);
-            }
+            } 
             return;
         }
-        for(int i=index; i<num.length(); i++){
-            if(i!=index && num[index]=='0'){
-                return;
-            }
+        for(int i=index; i<num.size(); i++){
+            if(i!=index && num[index]=='0')return;
+
             string currstr = num.substr(index, i-index+1);
             long currnum = stol(currstr);
+
             if(index==0){
-                solve(num, target, i+1, currnum, currnum, exp+currstr, result);
+                createexp(num, target, result, i+1, currnum, currnum, exp+currstr);
             }else{
-               solve(num, target, i+1, curr+currnum, currnum, exp+"+"+currstr, result);
-               solve(num, target, i+1, curr-currnum, -currnum, exp+"-"+currstr, result);
-               solve(num, target, i+1, curr-prev+prev*currnum, prev*currnum, exp+"*"+currstr, result); 
+                createexp(num, target, result, i+1, curr+currnum, currnum, exp+"+"+currstr);
+                createexp(num, target, result, i+1, curr-currnum, -currnum, exp+"-"+currstr);
+                createexp(num, target, result, i+1, curr-prev+prev*currnum, prev*currnum, exp+"*"+currstr);
             }
         }
-    }    
+    }
     vector<string> addOperators(string num, int target) {
         vector<string> result;
-        solve(num, target, 0, 0, 0, "", result);
+        createexp(num, target, result, 0, 0, 0, "");
         return result;
     }
 };
