50cf13fa0a3ab8ecd1ff2108453bdc0764f65cdd
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-15 17:42:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (14.17%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        double ans = 1.0;
-        long nn = n;
-        if(nn<0)nn= -1*nn;
-        while(nn>0){
-            if(nn%2==1){
-                ans = ans*x;
-                nn = nn-1;
-            }else{
-                x = x*x;
-                nn = nn/2;
-            }
+    double pow(double x, long n){
+        if(n==0){
+            return 1.0;
         }
-        if(n<0)return(double(1.0)/double(ans));
-        return(double(ans));
+        if(n==1){
+            return x;
+        }
+        if(n%2==0){
+            return pow(x*x,n/2);
+        }
+        return x*pow(x, n-1);
+    }
+    double myPow(double x, int num) {
+        long n = num;
+        if (n < 0) {
+            // Calculate the power of -n and take reciprocal
+            return (1.0 / pow(x, -1 * n));
+        }
+        return pow(x, n);
     }
 };
