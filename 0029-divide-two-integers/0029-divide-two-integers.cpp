6e1eb915c5d31a113960c609773ec1b0ceebcc2c
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-15 12:08:09 +0530

    Time: 0 ms (100%), Space: 8.6 MB (37.9%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1)
+        // Handling the INT_MAX
+        if(dividend==INT_MIN && divisor==-1){
             return INT_MAX;
+        }
 
-        bool positive = (dividend > 0) == (divisor > 0);
+        long n = labs(dividend);
+        long d = labs(divisor);
+        long q = 0;
 
-        long n = labs((long)dividend);
-        long d = labs((long)divisor);
+        // CHeck if it is +ve / -ve
+        bool pos = (dividend>0)==(divisor>0);
 
-        long quotient = 0;
-
-        while (n >= d) {
-            int count = 0;
+        // Try to represent divisor in terms of power 2
+        while(n>=d){
+            int count=0; // track how much time a divisor can be represented in terms of 2
             while (n >= (d << (count + 1))) {
                 count++;
             }
-            n -= d << count;
-            quotient += 1L << count;
+            q+=(1L<<count);
+            n-=(d<<count);
         }
+        if(!pos)q=-q;
 
-        if (!positive) quotient = -quotient;
+        if(q<INT_MIN)return INT_MIN;
+        if(q>INT_MAX)return INT_MAX;
 
-        if (quotient > INT_MAX) return INT_MAX;
-        if (quotient < INT_MIN) return INT_MIN;
-
-        return (int)quotient;
+        return int(q);
     }
 };
