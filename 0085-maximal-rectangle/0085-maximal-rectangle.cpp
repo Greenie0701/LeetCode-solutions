f4a7c6b5a39a09b5fe9703910f54256b27ddf12e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-15 18:05:37 +0530

    Time: 1 ms (91.63%), Space: 19.3 MB (45.32%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,41 +1,47 @@
 class Solution {
 public:
-    int max_area(vector<int>& arr){
+    int find_area(vector<int>& arr){
+        int max_area = 0;
         stack<int> s;
-        int maxi =0;
-        for(int i=0;i<arr.size();i++){
+        for(int i=0; i<arr.size(); i++){
             while(!s.empty()&&arr[s.top()]>arr[i]){
                 int nse = i;
-                int curr = arr[s.top()];
+                int prev =  arr[s.top()];
                 s.pop();
                 int pse = s.empty()?-1:s.top();
-                maxi = max(maxi, curr*(nse-pse-1));
+                max_area = max(max_area, prev*(nse-pse-1));
             }
             s.push(i);
         }
         while(!s.empty()){
             int ns = arr.size();
-            int cur = arr[s.top()];
+            int pre = arr[s.top()];
             s.pop();
             int ps = s.empty()?-1:s.top();
-            maxi = max(maxi, cur*(ns-ps-1));
+            max_area = max(max_area, pre*(ns-ps-1));
         }
-        return maxi;
+        return max_area;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         vector<vector<int>> prefix_sum(matrix.size(), vector<int>(matrix[0].size(), 0));
-        int maxi = 0;
-        for(int j=0; j<matrix[0].size(); j++){
-            int sum=0;
-            for(int i=0; i<matrix.size(); i++){
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int max_area =0;
+        for(int j=0; j<col; j++){
+            int sum = 0;
+            for(int i=0; i<row; i++){
                 sum+=matrix[i][j]-'0';
-                if(matrix[i][j]=='0')sum=0;
-                prefix_sum[i][j]=sum;
+                if(matrix[i][j]=='0'){
+                    prefix_sum[i][j]=0;
+                    sum = 0;
+                }else{
+                    prefix_sum[i][j] = sum;
+                }
             }
         }
-        for(int i=0; i<matrix.size(); i++){
-            maxi = max(maxi, max_area(prefix_sum[i]));
+        for(int i=0; i<row; i++){
+            max_area = max(max_area, find_area(prefix_sum[i]));
         }
-        return maxi;
+        return max_area;
     }
 };
