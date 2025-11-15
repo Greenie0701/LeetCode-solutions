79e1a22e62203862aafa4df2746ca3e22af99f95
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-15 15:06:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n <= 2) return 0;
-        
-        vector<bool> prime(n, true);
-        prime[0] = prime[1] = false;
-
-        for (int p = 2; p * p < n; p++) {
-            if (prime[p]) {
-                for (int i = p * p; i < n; i += p) {
-                    prime[i] = false;
+        vector<bool> prime(n+1, true);
+        prime[0] = false;
+        prime[1] = false;
+        for(int i=2; i*i<n; i++){
+            if(prime[i]){
+                for(int j=i*i; j<=n; j+=i){
+                    prime[j]=false;
                 }
             }
         }
-
         return count(prime.begin(), prime.end(), true);
     }
 };
