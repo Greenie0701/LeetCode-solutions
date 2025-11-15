a4b5ae29433e1eabd2d4ed628efae514a0c5ec07
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-15 20:16:58 +0530

    Time: 0 ms (100%), Space: 15.7 MB (67.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // Find the index where current index < index+1, break
         int index = -1;
-        int size = nums.size();
-        for(int i=size-2; i>=0; i--){
+        int size = nums.size()-1;
+        for(int i=size-1; i>=0; i--){
             if(nums[i]<nums[i+1]){
                 index = i;
                 break;
             }
         }
-        // If no such index is found, just return the reverse of the array
         if(index==-1){
             reverse(nums.begin(), nums.end());
             return;
         }
-        // Swap the index with first element from end where arr[index]<arr[i]
-        for(int i=size-1; i>index; i--){
+        for(int i=size; i>index; i--){
             if(nums[index]<nums[i]){
-                swap(nums[i], nums[index]);
+                swap(nums[index],nums[i]);
                 break;
             }
         }
-        // Reverse the array from [index+1] to n-1
+
         reverse(nums.begin() + index + 1, nums.end());
         return;
+
     }
 };
