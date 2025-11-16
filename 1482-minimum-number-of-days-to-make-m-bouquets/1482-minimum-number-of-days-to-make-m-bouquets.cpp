8595e11f5d940a0a9525f71fd1134513dd602b4d
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-16 18:49:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
-    bool possible(vector<int>& bloomDay, int n,  int m, int k){
-        int count =0;
-        int b = 0;
-        for(int i = 0;i<bloomDay.size();i++){
-            if(bloomDay[i]<=n){
-                count++;
-                if(count==k){
-                    b++;
-                    count =0;
+    bool possible(vector<int>& arr, int mid, int m, int k){
+        int num = 0;
+        int boq = 0;
+        for(int i=0; i<arr.size(); i++){
+            if (arr[i] <= mid) {
+                num++;
+                if (num == k) {
+                    boq++;
+                    num = 0;
                 }
-            }else{
-                count = 0;
+            } else {
+                num = 0; // break the consecutive chain
             }
         }
-        if(b>=m){
-            return true;
-        }else{
-            return false;
-        }
+        return(boq>=m);
     }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        long long val = m*1ll*(k)*1ll;
-        int ans = -1;
-        if( val > bloomDay.size()){
-            return ans;
+    int minDays(vector<int>& arr, int m, int k) {
+        if(long long(m)*k>arr.size()){
+            return -1;
         }
-        // flowers bloom in first day to last day - Range
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
-        int high = *max_element(bloomDay.begin(), bloomDay.end());
-        // Perform the binary search 
+        int ans =-1;
+        int low = *min_element(arr.begin(), arr.end());
+        int high = *max_element(arr.begin(), arr.end());
         while(low<=high){
-            int mid = low + (high-low)/2;
-            // Check with this mid can you make boque
-            if(possible(bloomDay, mid, m, k)){
+            int mid = low+(high-low)/2;
+            if(possible(arr, mid, m, k)){
                 ans = mid;
-                high = mid - 1;
-            }
-            else{
+                high = mid-1;
+            }else{
                 low = mid+1;
             }
         }
