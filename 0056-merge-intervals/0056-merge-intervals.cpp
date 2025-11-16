1363494d0b40b941575b9bbda690c296e1532b8e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-16 09:45:43 +0530

    Time: 15 ms (8.72%), Space: 25.8 MB (5.36%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // Create a 2D vector that contains non-overlapping intervals 
-        vector<vector<int>> ans;
-        // Sort the array 
-        sort(intervals.begin(),intervals.end());
-        // Iterate through the array
-        for(int i=0;i<intervals.size();i++){
-            // Check if the resultant array is empty or the previous interval is not part of current interval
-            if(ans.empty() || ans.back()[1]<intervals[i][0]){
-                ans.push_back(intervals[i]);
-            }
-            // Else my previous interval overlaps with my current interval, so replace the previous end with 
-            // max end of the current and previous intervals
-            else{
-                ans.back()[1]=max(intervals[i][1], ans.back()[1]);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> result;
+        for(auto i:intervals){
+            if(result.empty()||result.back()[1]<i[0]){
+                result.push_back(i);
+            }else{
+                vector<int> prev = result.back();
+                result.pop_back();
+                prev[1] = prev[1]>=i[1]?prev[1]:i[1];
+                result.push_back(prev);
             }
         }
-        return ans;
+        return result;
     }
 };
