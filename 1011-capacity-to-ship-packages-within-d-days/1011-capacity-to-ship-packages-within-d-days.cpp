da8dd710a9a6969df9a8d8cfebdb5a007dd4864f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-16 18:57:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    bool possible(vector<int>& weights, int k,  int days){
-        int d =1;
-        int current = 0;
-        for(int i=0; i<weights.size(); i++){
-            if (weights[i] > k) return false;
-            if(current+weights[i]>k){
-                d++;
-                current=weights[i];
+    bool possible(vector<int>& weights, int cap, int days){
+        int req=1;
+        int sum=0;
+        for(int n:weights){
+            if(sum+n<=cap){
+                sum+=n;
             }else{
-                current+=weights[i];
+                sum=n;
+                req++;
             }
         }
-        if(d<=days){
-            return true;
-        }else{
-            return false;
-        }
+        return req<=days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        // Find the range of capacity. It can hold either min element of capacity or max weight
         int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-        int ans = high;
-        // Find the minimum capacity where all weights can be shipped
+        int high = accumulate(weights.begin(), weights.end());
+        int ans=-1;
         while(low<=high){
-            int mid = low+(high - low)/2;
+            int mid = low+(high-low)/2;
             if(possible(weights, mid, days)){
                 ans = mid;
                 high = mid-1;
