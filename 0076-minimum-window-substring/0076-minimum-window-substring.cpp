ddc624c4c3739880e5b551c3457f54f4a96ae772
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-20 07:06:34 +0530

    Time: 7 ms (66.08%), Space: 11.2 MB (93.7%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int size = s.length();
+        int l = 0;
+        int r = 0;
+        int n= s.length();
         int m = t.length();
+        int count =0;
         int mini = INT_MAX;
         int start = -1;
-        for(int i =0; i<size; i++){
-            vector<int> hash(128, 0);
-            int count = 0;
-            for(int j=0; j<m; j++){
-                hash[t[j]]++;
-            }
-            for(int k=i; k<size; k++){
-                if(hash[s[k]]>0)count++;
-                hash[s[k]]--;
-                if(count==m){
-                    mini = min(mini, k-i+1);
-                    start = i;
-                    break;
+        vector<int> hash(128,0);
+        // Precompute the hash map for string t(Required to track the number of elements in s of t)
+        for(int i=0; i<m; i++){
+            hash[t[i]]++;
+        }
+        while(r<n){
+            // If the element is part of t, increment count
+            if(hash[s[r]]>0)count++;
+            // Decrement its count in hash, can be used to find its occurance in future;
+            hash[s[r]]--;
+            // Shrink the substring since you want to find the minimum length string
+            while(count==m){
+                if(mini>(r-l+1)){
+                    mini = r-l+1;
+                    start = l;
                 }
+                // Now shrink from the left;
+                hash[s[l]]++; // This means we are removing from the substring
+                if(hash[s[l]]>0)count--; // Need to find occurance of char at l in future
+                l++; //move left front
             }
+            r++;
         }
         return start==-1?"":s.substr(start, mini);
     }
