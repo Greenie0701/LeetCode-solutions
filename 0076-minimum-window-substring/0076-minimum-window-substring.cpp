69a63b1a113c0c7cef8980b97c08633c95c4c5c1
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-20 21:21:59 +0530

    Time: 2 ms (83.74%), Space: 11.4 MB (63.55%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        vector<int> hash(128, 0);
         int l = 0;
-        int r = 0;
-        int n= s.length();
-        int m = t.length();
-        int count =0;
+        int r =0;
         int mini = INT_MAX;
-        int start = -1;
-        vector<int> hash(128,0);
-        // Precompute the hash map for string t(Required to track the number of elements in s of t)
-        for(int i=0; i<m; i++){
+        int start =-1;
+        int end = s.length();
+        int n = t.length();
+        int count = 0;
+        for(int i=0; i<n; i++){
             hash[t[i]]++;
         }
-        while(r<n){
-            // If the element is part of t, increment count
+        while(r<end){
             if(hash[s[r]]>0)count++;
-            // Decrement its count in hash, can be used to find its occurance in future;
             hash[s[r]]--;
-            // Shrink the substring since you want to find the minimum length string
-            while(count==m){
+            while(count==n){
                 if(mini>(r-l+1)){
                     mini = r-l+1;
                     start = l;
                 }
-                // Now shrink from the left;
-                hash[s[l]]++; // This means we are removing from the substring
-                if(hash[s[l]]>0)count--; // Need to find occurance of char at l in future
-                l++; //move left front
+                hash[s[l]]++;
+                if(hash[s[l]]>0)count--;
+                l++;
             }
             r++;
         }
