51cb4c62a9f396b0e655e94ca15700f98bc89804
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-22 11:26:22 +0530

    Time: 0 ms (100%), Space: 11.3 MB (93.63%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        int count=0;
+        int mini=INT_MAX;
         vector<int> hash(128, 0);
-        int l = 0;
-        int r =0;
-        int mini = INT_MAX;
-        int start =-1;
+        int l1 = s.length();
+        int l2 = t.length();
+        int start = -1;
+        int l=0;
+        int r=0;
         int end = s.length();
-        int n = t.length();
-        int count = 0;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<l2; i++){
             hash[t[i]]++;
         }
         while(r<end){
             if(hash[s[r]]>0)count++;
             hash[s[r]]--;
-            while(count==n){
+            while(count==l2){
                 if(mini>(r-l+1)){
-                    mini = r-l+1;
+                    mini = (r-l+1);
                     start = l;
                 }
                 hash[s[l]]++;
