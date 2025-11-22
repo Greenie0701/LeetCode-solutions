ab5d3fb5505b05b28e96f4982c27b8d00f6b8340
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-22 19:53:59 +0530

    Time: 1 ms (14.69%), Space: 8.6 MB (71.63%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend==INT_MIN&&divisor==-1){
+        if(dividend == INT_MIN && divisor==-1){
             return INT_MAX;
         }
-        // Determine the sign of the number
-        bool pos_sign = dividend>0==divisor>0;
-
-        // take copy of the given divisor and dividend
+        bool sign = dividend>0==divisor>0;
         long n = labs(dividend);
         long d = labs(divisor);
-        long q =0;
+        long q = 0;
 
-        // Loop untill you can represent the given divisor in terms of power 2 to get near to the dividend
         while(n>=d){
             int count = 0;
             while(n>=(d<<(count+1))){
                 count++;
             }
-            n-=(d<<count);
-            q+=(1L<<count);
+            n-=(d<<(count));
+            q+=(1L<<(count));
         }
 
-        if(!pos_sign)q=-q;
-
-        if(q>INT_MAX)return INT_MAX;
+        if(!sign)q=-q;
         if(q<INT_MIN)return INT_MIN;
-        
+        if(q>INT_MAX)return INT_MAX;
         return q;
     }
 };
