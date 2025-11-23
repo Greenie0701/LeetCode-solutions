6fc4da375767036920a0b7e67b868de47707384e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-23 08:55:11 +0530

    Time: 0 ms (100%), Space: 9.1 MB (98.15%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        // Base case check whether both arrays are in same size
         if(s.length()!=t.length()){
             return false;
         }
-        // Use ascii array to track the elements occurance
-        int m1[256] = {0};
-        int m2[256] = {0};
-        // Iterate through every element
-        for(int i=0;i<=s.length()-1;i++){
-            if(m1[s[i]]!=m2[t[i]]){
+        vector<int> freq1(256, -1);
+        vector<int> freq2(256, -1);
+        for(int i=0; i<s.length(); i++){
+            if(freq1[s[i]]!=freq2[t[i]]){
                 return false;
             }
-            m1[s[i]] = i+1;
-            m2[t[i]] = i+1;
+            freq1[s[i]]=i;
+            freq2[t[i]]=i;
         }
         return true;
     }
