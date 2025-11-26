f82f00b479d6c30c3232e071b2f11fe339fd489e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-26 20:36:35 +0530

    Time: 20 ms (66.46%), Space: 81.4 MB (61.16%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
-    vector<int> findnse(vector<int>& heights, vector<int>& nse){
-        stack<int> s;
-        int size=heights.size();
-        for(int i=size-1; i>=0; i--){
-            while(!s.empty() && heights[s.top()]>=heights[i]){
-                s.pop();
-            }
-            if(!s.empty()){
-                nse[i] = s.top();
-            }
-            s.push(i);
-        }
-        return nse;
-    }
-    vector<int> findpse(vector<int>& heights, vector<int>& pse){
-        stack<int> s;
-        int size=heights.size();
-        for(int i=0; i<size; i++){
-            while(!s.empty() && heights[s.top()]>=heights[i]){
-                s.pop();
-            }
-            if(!s.empty()){
-                pse[i] = s.top();
-            }
-            s.push(i);
-        }
-        return pse;
-    }
     int largestRectangleArea(vector<int>& heights) {
-        int size = heights.size();
-        vector<int> nse(size, size);
-        vector<int> pse(size, -1);
-        nse = findnse(heights, nse);
-        pse = findpse(heights, pse);
         int max_area = 0;
+        stack<int> s;
+        int nse = 0;
+        int pse = 0;
         for(int i=0; i<heights.size(); i++){
-            max_area = max(max_area, heights[i]*(nse[i]-pse[i]-1));
+            while(!s.empty()&&heights[s.top()]>heights[i]){
+                int prev = heights[s.top()];
+                s.pop();
+                nse = i;
+                pse = s.empty()?-1:s.top();
+                max_area = max(max_area, prev*(nse-pse-1));
+            }
+            s.push(i);
+        }
+        while(!s.empty()){
+            nse = heights.size();
+            int element = heights[s.top()];
+            s.pop();
+            pse = s.empty()?-1:s.top();
+            max_area = max(max_area, element*(nse-pse-1));
         }
         return max_area;
     }
