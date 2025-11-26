2e9613e7d38cca98c4419452fb745a8dbad62d27
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-26 20:42:59 +0530

    Time: 0 ms (100%), Space: 21.5 MB (88.34%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -6,15 +6,12 @@ public:
             if(asteroids[i]>0){
                 result.push_back(asteroids[i]);
             }else{
-                // Pop all asteroids which are lesser than current asteroid
                 while(!result.empty()&&result.back()>0&&result.back()<abs(asteroids[i])){
                     result.pop_back();
                 }
-                // Check if the size of asteroids are equal
-                if(!result.empty()&&result.back()==abs(asteroids[i])){
+                if(!result.empty()&&result.back()>0&&result.back()==abs(asteroids[i])){
                     result.pop_back();
                 }
-                // Push the asteroid to the array if it is neg/stack is empty
                 else if(result.empty()||result.back()<0){
                     result.push_back(asteroids[i]);
                 }
