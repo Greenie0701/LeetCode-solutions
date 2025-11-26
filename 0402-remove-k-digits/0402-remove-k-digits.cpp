07a2c9adfccc08fc9b5b2f6d3d0158f3e70e8a54
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-26 20:51:07 +0530

    Time: 5 ms (34.43%), Space: 11.1 MB (90.77%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    string removeKdigits(string num, int k) {
+    string removeKdigits(string nums, int k) {
         string s;
-        if(num.length()==k){
-            return "0";
-        }
-        for(int i=0; i<num.length();i++){
-            while(!s.empty()&&k>0&&s.back()-'0'>num[i]-'0'){
+        for(int i=0; i<nums.size(); i++){
+            while(!s.empty()&&k>0&&s.back()-'0'>nums[i]-'0'){
                 s.pop_back();
                 k--;
             }
-            s.push_back(num[i]);
+            s.push_back(nums[i]);
         }
-        while(k>0){
+        while(k!=0){
             s.pop_back();
             k--;
         }
-        int index = 0;
-        while(index<s.length()&&s[index]=='0')index++;
-        s =s.substr(index);
-        return s.empty()?"0":s;
+        int start = 0;
+        while(s[start]=='0'&&start<s.length())start++;
+        return start==s.length()?"0":s.substr(start);
     }
 };
