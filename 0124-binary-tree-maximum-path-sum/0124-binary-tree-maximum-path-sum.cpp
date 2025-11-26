c38003003dc958051383227d726e24d6bf30442f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-26 17:58:05 +0530

    Time: 0 ms (100%), Space: 28 MB (47.93%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,24 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxpathsum(TreeNode* root, int& maxi) {
-        if (root == NULL) {
+    int findmaxsum(TreeNode* root,int& maxi){
+        if(root==NULL){
             return 0;
         }
-
-        // Compute left and right subtree sums, but ignore negatives
-        int leftsum = max(0, maxpathsum(root->left, maxi));
-        int rightsum = max(0, maxpathsum(root->right, maxi));
-
-        // Update global maximum
-        maxi = max(maxi, root->val + leftsum + rightsum);
-
-        // Return the best one-side path sum (for parent use)
-        return root->val + max(leftsum, rightsum);
+        int leftsum = max(0, findmaxsum(root->left, maxi));
+        int rightsum = max(0,findmaxsum(root->right, maxi));
+        
+        maxi = max(maxi, root->val+leftsum+rightsum);
+        return root->val+max(leftsum, rightsum);
     }
-
     int maxPathSum(TreeNode* root) {
         int maxi = INT_MIN;
-        maxpathsum(root, maxi);
+        findmaxsum(root, maxi);
         return maxi;
     }
 };
