acaf62e4bccaef14c51ea4b109a520b8a1121c88
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-26 20:59:11 +0530

    Time: 0 ms (100%), Space: 25.9 MB (79.13%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int l = 0;
         int r = height.size()-1;
+        int l = 0;
         int maxl = height[l];
         int maxr = height[r];
         int max_area = 0;
@@ -10,13 +10,13 @@ public:
             if(maxl<maxr){
                 l++;
                 maxl = max(maxl, height[l]);
-                max_area+=maxl-height[l];
+                max_area+= maxl-height[l];
             }else{
                 r--;
                 maxr = max(maxr, height[r]);
-                max_area+=maxr-height[r];
+                max_area+= maxr-height[r];
             }
-        } 
+        }
         return max_area;
     }
 };
