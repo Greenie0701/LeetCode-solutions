fc60dbfa1f73c1bf9f440e9372844c745017282a
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-26 20:25:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.88%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend == INT_MIN && divisor==-1){
+        if(dividend==INT_MIN && divisor==-1){
             return INT_MAX;
         }
-        bool sign = dividend>0==divisor>0;
+
+        bool pos = dividend>0==divisor>0;
+
         long n = labs(dividend);
         long d = labs(divisor);
         long q = 0;
@@ -14,13 +16,14 @@ public:
             while(n>=(d<<(count+1))){
                 count++;
             }
-            n-=(d<<(count));
-            q+=(1L<<(count));
+            q+=(1L<<count);
+            n-=(d<<count);
         }
+        if(!pos)q=-q;
 
-        if(!sign)q=-q;
         if(q<INT_MIN)return INT_MIN;
         if(q>INT_MAX)return INT_MAX;
+
         return q;
     }
 };
