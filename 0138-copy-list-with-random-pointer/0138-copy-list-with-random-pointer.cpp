95172f67280e19df3ec1d2c15780edc3e17b3af0
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-29 10:44:45 +0530

    Time: 5 ms (64.66%), Space: 15.1 MB (70.93%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,48 +16,47 @@ public:
 
 class Solution {
 public:
-
-    void createrandomnode(Node* head){
-        Node* temp = head;
-        while(temp!=NULL){
-            Node* copy = temp->next;
-            if(temp->random==NULL){
-                copy->random = NULL;
-            }else{
-                copy->random = temp->random->next;
-            }
-            temp = copy->next;
-        }
-        return;
-    }
     void createcopynode(Node* head){
         Node* temp = head;
-        Node* nextnode = NULL;
         while(temp!=NULL){
-            nextnode = temp->next;
-            Node* copy = new Node(temp->val);
-            temp->next = copy;
-            copy->next = nextnode;
-            temp = copy->next;
-        } 
-        return;
+            Node* copynode = new Node(temp->val);
+            Node* nextnode = temp->next;
+            temp->next = copynode;
+            copynode->next = nextnode;
+            temp = nextnode;
+        }
     }
-    Node* extractcopynode(Node* head){
+
+    void linkrandomnode(Node* head){
+        Node* temp = head;
+        while(temp!=NULL){
+            Node* copynode = temp->next;
+            if(temp->random==NULL){
+                copynode->random = NULL;
+            }else{
+                copynode->random = temp->random->next;
+            }
+            temp = copynode->next;
+        }
+    }
+
+    Node* extracthead(Node* head){
         Node* dummy = new Node(-1);
         Node* tail = dummy;
         Node* temp = head;
         while(temp!=NULL){
             tail->next = temp->next;
-            temp->next  = temp->next->next;
+            temp->next = temp->next->next;
             temp = temp->next;
             tail = tail->next;
         }
         return dummy->next;
     }
+
     Node* copyRandomList(Node* head) {
         createcopynode(head);
-        createrandomnode(head);
-        Node* newhead = extractcopynode(head);
+        linkrandomnode(head);
+        Node* newhead = extracthead(head);
         return newhead;
     }
 };
