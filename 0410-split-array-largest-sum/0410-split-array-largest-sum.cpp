4c366530e980714f02c155988b7f7fac102d6edc
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-29 10:53:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (89.23%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,36 +1,38 @@
 class Solution {
 public:
-    bool check_possible(vector<int>& nums, int mid, int k){
-        int count_subarray = 1;
+
+    bool possible(vector<int>& nums, int k, int limit){
         int sum = 0;
+        int num = 1;
         for(int i=0; i<nums.size(); i++){
-            if(sum+nums[i]<=mid){
+            if(sum+nums[i]<=limit){
                 sum+=nums[i];
-            }
-            else{
-                count_subarray++;
+            }else if(num<k){
+                num++;
                 sum=nums[i];
+            }else{
+                return false;
             }
         }
-        if(count_subarray<=k){
-            return true;
-        }else{
-            return false;
-        }
+        return true;
     }
+
     int splitArray(vector<int>& nums, int k) {
         int low = *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(), nums.end(), 0);
+        int ans = -1;
         while(low<=high){
             int mid = low+(high-low)/2;
 
-            if(check_possible(nums, mid, k)){
+            if(possible(nums, k, mid)){
+                ans = mid;
                 high = mid-1;
             }
             else{
                 low = mid+1;
             }
+
         }
-        return low;
+        return ans;
     }
 };
