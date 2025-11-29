eba2cc6e76d7625b9e8824a582d8db11dd331080
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-29 10:27:57 +0530

    Time: 0 ms (100%), Space: 16.5 MB (69.69%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,52 +10,46 @@
  */
 class Solution {
 public:
-    ListNode* getkthNode(ListNode* head, int k){
-        k--;
-        ListNode* temp = head;
-        while(temp!=NULL && k>0){
-            k--;
-            temp=temp->next;
-        }
-        return temp;
-    }
     ListNode* reverse(ListNode* head){
         ListNode* curr = head;
         ListNode* prev = NULL;
         while(curr!=NULL){
             ListNode* next = curr->next;
             curr->next = prev;
-            prev=curr;
-            curr=next;
+            prev = curr;
+            curr = next;
         }
         return prev;
     }
+    ListNode* getknode(ListNode* temp, int k){
+        k--;
+        while(temp!=NULL&&k>0){
+            k--;
+            temp=temp->next;
+        }
+        return temp;
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* prevlast = NULL;
         ListNode* temp = head;
-        ListNode* prev = NULL;
         while(temp!=NULL){
-            // Determine the Kth Node, if not exist attach Prev chuck to curr temp, end
-            ListNode* kthnode = getkthNode(temp, k);
+            ListNode* kthnode = getknode(temp, k);
             if(kthnode==NULL){
-                if(prev!=NULL){
-                    prev->next = temp;
+                if(prevlast!=NULL){
+                    prevlast->next=temp;
                 }
                 break;
             }
-            // Once you get the kth Node reverse the chunck and make sure to preserve the next node.
-            ListNode* next = kthnode->next;
+            ListNode* nextnode = kthnode->next;
             kthnode->next = NULL;
             ListNode* reversehead = reverse(temp);
-            // Check if this the first chunck of given LL
             if(temp==head){
-                head = kthnode;
+                head=reversehead;
+            }else{
+                prevlast->next = reversehead;
             }
-            // Else attach the reversed head to the prev chunck
-            else{
-                prev->next = reversehead;
-            }
-            prev = temp;
-            temp = next;
+            prevlast = temp;
+            temp = nextnode;
         }
         return head;
     }
