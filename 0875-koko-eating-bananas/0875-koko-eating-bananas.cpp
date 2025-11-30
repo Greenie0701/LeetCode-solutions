e90facbce055aed39d03c51961b2aa7417443185
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-30 17:55:58 +0530

    Time: 11 ms (41.25%), Space: 22.9 MB (44.66%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     bool possible(vector<int>& piles, int k, int h){
-        int size = piles.size();
-        long long hours = 0;
-        for(int i = 0; i<size; i++){
-            hours+=ceil(double(piles[i])/double(k));
+        int n = piles.size();
+        long long total = 0;
+        for(int i=0; i<n; i++){
+            total+=ceil(double(piles[i])/double(k));
         }
-        if(hours<=h){
+        if(total<=h){
             return true;
-        }
-        else{
+        }else{
             return false;
         }
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        // Initialise the search space
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
         int ans = -1;
-        // Perform binary search to 
         while(low<=high){
             int mid = low+(high-low)/2;
             if(possible(piles, mid, h)){
                 ans = mid;
                 high = mid-1;
-            }
-            else{
+            }else{
                 low = mid+1;
             }
         }
