643abfead732a26920e89d7c741b73a05ba60694
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-11-30 07:27:22 +0530

    Time: 0 ms (100%), Space: 50.3 MB (90.37%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    int maxindex(vector<vector<int>>& mat, int mid){
-        int maxi = mat[0][mid];
-        int index = 0;
-        for(int i=1; i<mat.size(); i++){
-            if(mat[i][mid]>maxi){
-                maxi = mat[i][mid];
-                index = i;
+
+    int findmaxrow(vector<vector<int>>& mat, int col){
+        int maxi = INT_MIN;
+        int row = -1;
+        for(int i=0; i<mat.size(); i++){
+            if(mat[i][col]>maxi){
+                maxi = mat[i][col];
+                row = i;
             }
         }
-        return index;
+        return row;
     }
+
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int m = mat[0].size();
-        int n = mat.size();
         int low = 0;
-        int high = m-1;
+        int high = mat[0].size()-1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            int maxrowindex = maxindex(mat, mid);
-            int right = mid+1 >= m ? -1:mat[maxrowindex][mid+1];
-            int left = mid-1 < 0 ? -1:mat[maxrowindex][mid-1];
-            if(left<mat[maxrowindex][mid] && right< mat[maxrowindex][mid]){
-                return {maxrowindex, mid};
+
+            int row = findmaxrow(mat, mid);
+
+            int left = mid-1<0?-1:mat[row][mid-1];
+            int right = mid+1>high?-1:mat[row][mid+1];
+
+            if(left<mat[row][mid]&&right<mat[row][mid]){
+                return{row, mid};
             }
-            else if(left>mat[maxrowindex][mid]){
+            else if(left>mat[row][mid]){
                 high = mid-1;
             }
-            else if(right>mat[maxrowindex][mid]){
+            else{
                 low = mid+1;
             }
         }
-        return {-1,-1};
+        return{-1,-1};
     }
 };
