02e79a85b11f5abd80fed97ed31f777fbe9ef302
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-01 06:24:43 +0530

    Time: 0 ms (100%), Space: 95.1 MB (61.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,31 +3,29 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
-
-        if(n1>n2){
+        if(n2<n1){
             return findMedianSortedArrays(nums2, nums1);
         }
-
-        int total = n1+n2;
-        int half = (total+1)/2;
-
         int low = 0;
         int high = n1;
+        int total = n1+n2;
+        int half = (total+1)/2;
 
         while(low<=high){
             int m1 = low+(high-low)/2;
             int m2 = half-m1;
 
-            int l1 = (m1-1)<0?INT_MIN:nums1[m1-1]; 
-            int l2 = (m2-1)<0?INT_MIN:nums2[m2-1];
+            int l1 = m1-1<0?INT_MIN:nums1[m1-1];
+            int l2 = m2-1<0?INT_MIN:nums2[m2-1];
             int r1 = m1>=n1?INT_MAX:nums1[m1];
             int r2 = m2>=n2?INT_MAX:nums2[m2];
 
             if(l1<=r2&&l2<=r1){
-                if((total&1)==1){
-                    return(double(max(l1, l2)));
-                }else{
+                if(total%2==0){
                     return((max(l1,l2)+min(r1,r2))/2.0);
+                }
+                else{
+                    return(double(max(l1,l2)));
                 }
             }
             else if(l1>r2){
