ab45ab5131042ae368ca895c617033e3b48cb8d0
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-02 11:16:38 +0530

    Time: 39 ms (95.31%), Space: 18.6 MB (91.66%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,7 +4,7 @@ public:
         int row = 0;
         int col = matrix[0].size()-1;
 
-        while((col>=0&&row<matrix.size())){
+        while((row<matrix.size()&&col>=0)){
             if(matrix[row][col]==target){
                 return true;
             }
