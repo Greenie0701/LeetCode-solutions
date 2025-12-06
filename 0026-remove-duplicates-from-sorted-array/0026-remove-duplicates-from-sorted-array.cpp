6682dd7c6125008f61f3c3851c319f0760d353ca
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-06 15:57:31 +0530

    Time: 0 ms (100%), Space: 22.6 MB (78.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        //Create an i index to track unique element
-        int i =0;
-        //iterate through the array to find unique elements
-        for(int j=1; j<nums.size();j++){
-            //Check whether the current element is unique
-            if(nums[i]!=nums[j]){
-                //If yes we found a unique key, so increment i
-                i++;
-                //Now store the jth element in ith index to track next unique element
-                nums[i]=nums[j];
-            }
+        int index =0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[index]!=nums[i]){
+                index++;
+                nums[index]=nums[i];
         }
-        //Zero indexing, so add 1 while returning
-        return(i+1);
+    }
+    return index+1;
     }
 };
