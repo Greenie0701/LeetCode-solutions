6ef351c1647cb8600d7e1f14cb94d21c7b546a70
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-09 07:41:23 +0530

    Time: 455 ms (11.21%), Space: 93.3 MB (48.4%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    void createexp(string num, int target, vector<string>& result, int index, long curr, long prev, string exp){
-        if(index==num.size()){
+    void find(string num, int target, vector<string>& result, long curr, long prev, int index,string exp){
+        if(index==num.length()){
             if(curr==target){
                 result.push_back(exp);
-            } 
+            }
             return;
         }
-        for(int i=index; i<num.size(); i++){
+        for(int i=index; i<num.length(); i++){
             if(i!=index && num[index]=='0')return;
-
             string currstr = num.substr(index, i-index+1);
             long currnum = stol(currstr);
-
             if(index==0){
-                createexp(num, target, result, i+1, currnum, currnum, exp+currstr);
+                find(num, target, result, currnum, currnum, i+1, exp+currstr);
             }else{
-                createexp(num, target, result, i+1, curr+currnum, currnum, exp+"+"+currstr);
-                createexp(num, target, result, i+1, curr-currnum, -currnum, exp+"-"+currstr);
-                createexp(num, target, result, i+1, curr-prev+prev*currnum, prev*currnum, exp+"*"+currstr);
+                find(num, target, result, curr+currnum, currnum, i+1, exp+'+'+currstr);
+                find(num, target, result, curr-currnum, -currnum, i+1, exp+'-'+currstr);
+                find(num, target, result, curr-prev+prev*currnum, prev*currnum, i+1, exp+'*'+currstr);
             }
         }
     }
     vector<string> addOperators(string num, int target) {
         vector<string> result;
-        createexp(num, target, result, 0, 0, 0, "");
+        find(num, target, result, 0, 0, 0, "");
         return result;
     }
 };
