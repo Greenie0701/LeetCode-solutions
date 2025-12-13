276a696a5753a59526b1ab32705f76dfcc907b51
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-13 17:24:49 +0530

    Time: 6 ms (5.21%), Space: 17.9 MB (5.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans="";
-        // The first loop is going to iterate untill length of first array
+        string s="";
         for(int i=0; i<strs[0].length(); i++){
-            // Iterate every string in string vector
-            for(string str : strs){
-                // Check if it is out of bounds or the char in the string are not eq
-                // If this is the case return ans
-                if(i==str.length() || str[i]!=strs[0][i]){
-                    return ans;
+            for(string str:strs){
+                if(i==str.length()||strs[0][i]!=str[i]){
+                    return s;
                 }
             }
-            // If not, the chars in the string is equal 
-            ans+=strs[0][i];
+            s+=strs[0][i];
         }
-        return ans;
+        return s;
     }
 };
