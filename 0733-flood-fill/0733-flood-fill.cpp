1e1b9fc05146a0614d482864b5f5bb1be6a7fbe6
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-17 19:12:09 +0530

    Time: 0 ms (100%), Space: 18.5 MB (14.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-
-    void dfs(vector<vector<int>>& image, vector<vector<int>>& result, int sr, int sc, int ini_color, int color, 
-    vector<int>& drow, vector<int>& dcol){
-        result[sr][sc]=color;
+    
+    void dfs(vector<vector<int>>& copy, vector<vector<int>>& image, vector<int> drow, vector<int> dcol,
+    int sr, int sc, int ini_color, int color){
+        copy[sr][sc] = color;
         for(int i=0; i<4; i++){
-            int row = sr+drow[i];
-            int col = sc+dcol[i];
-            if(row>=0 && row<image.size() && col>=0 && col<image[0].size() &&
-            result[row][col]!=color&&image[row][col]==ini_color){
-                dfs(image, result, row, col, ini_color, color, drow, dcol);
+            int r = sr+drow[i];
+            int c = sc+dcol[i];
+            if(r>=0&&r<image.size()&&c>=0&&c<image[0].size()&&image[r][c]==ini_color&&copy[r][c]!=color){
+                dfs(copy, image, drow, dcol, r, c, ini_color, color);
             }
         }
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) return image;
-        vector<vector<int>> result = image;
-        int Ini_color = image[sr][sc];
+        if(image[sr][sc]==color)return image;
+        vector<vector<int>> copy = image;
+        int ini_color = image[sr][sc];
         vector<int> drow = {1, -1, 0, 0};
-        vector<int> dcol = {0 ,0 , 1, -1};
-        dfs(image, result, sr, sc, Ini_color, color, drow, dcol);
-        return result;
-    } 
+        vector<int> dcol = {0, 0, 1, -1};
+        dfs(copy, image, drow, dcol, sr, sc, ini_color, color);
+        return copy;
+    }
 };
