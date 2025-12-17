479d6c4b14591fd986944402d5588028f4b4ffc6
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-17 18:56:29 +0530

    Time: 0 ms (100%), Space: 17.3 MB (20.36%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -3,49 +3,42 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-        // Visited array is just to mark them they are rotten already
-        vector<vector<int>> visited(row, vector<int>(col, 0));
-        // Queue ds to track {x, y} and time taken to rotten the grid
+        vector<vector<int>> rotten(row, vector<int>(col, 0));
+        int time = 0;
         queue<pair<pair<int, int>, int>> q;
-        // Time taken initially is zero
-        int time = 0; 
-        // Bush all rotten fruits initially in the Grid
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j}, 0});
-                    visited[i][j]=2;
+                    rotten[i][j]=2;
+                    q.push({{i, j},0});
                 }
             }
         }
-        // Perform BFS to rotten all oranges
+        vector<int> drow = {1, -1, 0, 0};
+        vector<int> dcol = {0, 0, 1, -1};
         while(!q.empty()){
-            auto it = q.front();
+            auto rot = q.front();
             q.pop();
-            int r = it.first.first;
-            int c = it.first.second;
-            int t = it.second;
-            time = max(t, time);
-            vector<int> drow = {0, 0, 1, -1};
-            vector<int> dcol = {1, -1, 0, 0};
+            int r = rot.first.first;
+            int c = rot.first.second;
+            int t = rot.second;
+            time = max(time, t);
             for(int i=0; i<4; i++){
                 int nrow = r+drow[i];
                 int ncol = c+dcol[i];
-                if(nrow>=0&&nrow<row&&ncol>=0&&ncol<col&&visited[nrow][ncol]!=2&&grid[nrow][ncol]==1){
-                    q.push({{nrow, ncol},t+1});
-                    visited[nrow][ncol]=2;
+                if(nrow>=0&&nrow<row&&ncol>=0&&ncol<col&&rotten[nrow][ncol]!=2&&grid[nrow][ncol]==1){
+                    rotten[nrow][ncol] =2;
+                    q.push({{nrow, ncol}, t+1});
                 }
             }
         }
-        // Check the visited array if all oranges are rotten 
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(grid[i][j]==1&&visited[i][j]!=2){
+                if(grid[i][j]==1&&rotten[i][j]!=2){
                     return -1;
                 }
             }
         }
-
         return time;
     }
 };
