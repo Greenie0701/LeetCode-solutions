449abdc744ba06fb9798fe278b8ee53a3d3f289b
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-20 18:30:03 +0530

    Time: 13 ms (43.44%), Space: 17.5 MB (16.83%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -13,6 +13,7 @@ public:
         if(root==NULL||p->val==root->val||q->val==root->val){
             return root;
         }
+
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
 
