f2ade8acf89fb6fdbb342267f577eb5f08103698
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-23 08:07:22 +0530

    Time: 14 ms (55.09%), Space: 13.1 MB (74.38%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.cpp
+++ 0126-word-ladder-ii/0126-word-ladder-ii.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    unordered_map<string, int> mpp;
+
+    unordered_map<string, int> transform;
     vector<vector<string>> result;
     string b;
+    int len;
 
     void dfs(string word, vector<string>& seq){
         if(word==b){
@@ -11,55 +13,53 @@ public:
             reverse(seq.begin(), seq.end());
             return;
         }
-        int step = mpp[word];
-        for(int i=0; i<word.length(); i++){
-            char org = word[i];
-            for(char c='a'; c<='z'; c++){
-                word[i]=c;
-                if(mpp.find(word)!=mpp.end()&&mpp[word]+1==step){
+        int step = transform[word];
+        for(int i=0; i<len; i++){
+            char ch = word[i];
+            for(char c = 'a'; c<='z'; c++){
+                word[i] = c;
+                if(transform.find(word)!=transform.end()&&transform[word]+1==step){
                     seq.push_back(word);
                     dfs(word, seq);
                     seq.pop_back();
                 }
-                word[i] = org; 
             }
+            word[i]=ch;
         }
     }
 
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> s(wordList.begin(), wordList.end());
+        b = beginWord;
         queue<pair<string, int>> q;
-        q.push({beginWord, 1});
+        q.push({beginWord, 0});
+        unordered_set<string> s(wordList.begin(), wordList.end());
         s.erase(beginWord);
-        b=beginWord;
-        int len = beginWord.length();
-        mpp[beginWord]=1;
-        int step = 1;
+        transform[beginWord] = 0;
+        len = beginWord.length();
         while(!q.empty()){
             string word = q.front().first;
-            step = q.front().second;
+            int st = q.front().second;
             q.pop();
-            if(word==endWord){
-                break;
-            }
+            if(word==endWord)break;
             for(int i=0; i<len; i++){
-                char org = word[i];
+                char ch = word[i];
                 for(char c='a'; c<='z'; c++){
                     word[i] = c;
                     if(s.find(word)!=s.end()){
-                        q.push({word, step+1});
                         s.erase(word);
-                        mpp[word]=step+1;
+                        q.push({word, st+1});
+                        transform[word] = st+1;
                     }
                 }
-                word[i] = org;
+                word[i] = ch;
             }
         }
-        if(mpp.find(endWord)!=mpp.end()){
+        if(transform.find(endWord) != transform.end()){
             vector<string> seq;
             seq.push_back(endWord);
             dfs(endWord, seq);
         }
+
         return result;
     }
 };
