b6ddfba972943b6d2e5c0a4c2df15417f1458f26
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-27 09:44:30 +0530

    Time: 23 ms (41.73%), Space: 36 MB (59.72%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int row = mat.size();
-        int col = mat[0].size();
-        vector<vector<int>> dist(row, vector<int>(col, -1));
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> matrix(m, vector<int>(n, -1));
         queue<pair<pair<int, int>, int>> q;
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(mat[i][j]==0){
-                    dist[i][j]=0;
-                    q.push({{i,j},0});
+                    matrix[i][j] = 0;
+                    q.push({{i, j}, 0});
                 }
             }
         }
         vector<int> drow = {1, -1, 0, 0};
         vector<int> dcol = {0, 0, 1, -1};
         while(!q.empty()){
-            auto val = q.front();
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int d = q.front().second;
             q.pop();
-            int r = val.first.first;
-            int c = val.first.second;
-            int d = val.second;
             for(int i=0; i<4; i++){
-                int nrow = r+drow[i];
-                int ncol = c+dcol[i];
-                if(nrow>=0&&nrow<row&&ncol>=0&&ncol<col&&dist[nrow][ncol]==-1&&mat[nrow][ncol]==1){
-                    dist[nrow][ncol]=d+1;
-                    q.push({{nrow, ncol}, d+1});
+                int row = r+drow[i];
+                int col = c+dcol[i];
+                if(row>=0&&row<m&&col>=0&&col<n&&matrix[row][col]==-1){
+                    q.push({{row, col}, d+1});
+                    matrix[row][col] = d+1;
                 }
             }
         }
-        return dist;
+        return matrix;
     }
 };
