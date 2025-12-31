7c70c373e842e3dce1e60c7a69d6c57f9f1d580f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2025-12-31 09:43:16 +0530

    Time: 54 ms (59.33%), Space: 24.7 MB (45.2%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,46 +1,37 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-        // Use Dijisktra to find the minimum path required to reach (rows-1, columns-1) from (0, 0)
-        // Initialise Priority queue {dist, {row, col}}
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        // Create Distance vector
-        int row = heights.size();
-        int col = heights[0].size();
-        vector<vector<int>> dist(row, vector<int>(col, INT_MAX));
-        dist[0][0] = 0;
-        pq.push({0, {0, 0}});
+        // Initialise PQ for performing dijisktra
+        priority_queue<pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>,greater<pair<int, pair<int, int>>>> pq;
+        // Create a dist graph
+        int m = heights.size();
+        int n = heights[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        // Initialise the dist and pq with source;
+        dist[0][0]=0;
+        pq.push({0,{0, 0}});
+        vector<int> drow = {1, -1, 0 ,0};
+        vector<int> dcol = {0, 0, 1, -1};
         // Perform BFS
-        int d = INT_MAX;
-        // Initialise the directions 
-        vector<int> drow ={1, -1, 0, 0};
-        vector<int> dcol ={0, 0, 1, -1};
         while(!pq.empty()){
-            // Get the distance
             int d = pq.top().first;
-            // Get the co-ordinates
-            int r= pq.top().second.first;
-            int c= pq.top().second.second;
-            // If source is reached, end the iteration
-            if(r==row-1&&c==col-1)return d;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            // Check if this a dest
+            if(row==m-1&&col==n-1)return d;
             pq.pop();
-            // Estimate the effort in all for directions
             for(int i=0; i<4; i++){
-                int nrow = r+drow[i];
-                int ncol = c+dcol[i];
-                // Check if the path is within bounds
-                if(nrow>=0&&ncol>=0&&nrow<row&&ncol<col){
-                    // Estimate the effort required
-                    int effort = abs(heights[r][c]-heights[nrow][ncol]);
-                    int neweffort = max(d, effort);
-                    // Check if the effort is less than the previous, if yes update 
-                    if(neweffort<dist[nrow][ncol]){
-                        dist[nrow][ncol] = neweffort;
-                        pq.push({neweffort, {nrow, ncol}});
+                int r = row+drow[i];
+                int c = col+dcol[i];
+                if(r>=0&&c>=0&&r<m&&c<n){
+                    int neweffort = max(d, abs(heights[row][col]-heights[r][c]));
+                    if(neweffort<dist[r][c]){
+                        dist[r][c]=neweffort;
+                        pq.push({neweffort,{r, c}});
                     }
                 }
             }
         }
-        return d;
+        return INT_MAX;
     }
 };
