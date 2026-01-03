a86e8b2cbdf62ae4aebeb38d13595160e595e0f0
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-03 11:14:48 +0530

    Time: 34 ms (13.7%), Space: 21.1 MB (10.15%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,42 +1,49 @@
 class Solution {
 public:
+
+    vector<int> dijisktra(int n, vector<vector<pair<int, int>>>& graph, int src){
+        // Create a distance vector
+        vector<int> dist(n, INT_MAX);
+        dist[src]=0;
+        // Perform the dijistra 
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, src});
+        while(!pq.empty()){
+            auto val = pq.top();
+            pq.pop();
+            int d = val.first;
+            int node = val.second;
+            // Iterate through the adj of nodes
+            for(auto adj:graph[node]){
+                int adjnode = adj.first;
+                int wt = adj.second;
+                if(dist[adjnode]>d+wt){
+                    dist[adjnode]=d+wt;
+                    pq.push({dist[adjnode], adjnode});
+                }
+            }
+        }
+        return dist;
+    }
+
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // Create adj matrix
-        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
-        // Set diagnol dist as zero
-        for(int i=0; i<n; i++){
-            dist[i][i]=0;
-        }
-        // Initialise the dist with given edges
+        int city = -1;
+        int citycnt = INT_MAX;
+        vector<vector<pair<int, int>>> graph(n);
         for(auto e:edges){
-            dist[e[0]][e[1]]=e[2];
-            dist[e[1]][e[0]]=e[2];
+            graph[e[0]].push_back({e[1],e[2]});
+            graph[e[1]].push_back({e[0],e[2]});
         }
-        // Perform the bellmanford algo
-        for(int k=0; k<n; k++){ // Find path via intermediate node k
-            for(int i=0; i<n; i++){
-                for(int j=0; j<n; j++){ // Iterate through the position(i, j)
-                    // Check if there exists a valid path from i to k and then k to j
-                    if(dist[i][k]!=INT_MAX && dist[k][j]!=INT_MAX){
-                        // Assign the shortest path by comparing via path k with existing path
-                        dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
-                    }
-                }
-            }
-        }
-        // Now check the city with shortest path less than threshold
-        int citycnt = INT_MAX; // Tracks the city with minimum city count within threshold
-        int city = -1; // City with minimum city count
         for(int i=0; i<n; i++){
-            int cnt=0; // Track the number of cities less than threshold
-            for(int j=0; j<n; j++){
-                if(dist[i][j]<=distanceThreshold&&i!=j){
-                    cnt++;
+            int count = 0;
+            vector<int> dist = dijisktra(n, graph, i);
+            for (int j = 0; j < n; j++) {
+                if (j != i && dist[j] <= distanceThreshold) {
+                    count++;
                 }
             }
-            // Found new city with smaller count or greater city with equal count
-            if(cnt<=citycnt){
-                citycnt = cnt;
+            if(count<=citycnt){
+                citycnt=count;
                 city = i;
             }
         }
