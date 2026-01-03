cbf769ad0a654e8b466748e0647ff0fb8156d837
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-03 18:33:30 +0530

    Time: 4 ms (48.58%), Space: 18.3 MB (50.87%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        
-        // Create the graph {dest, price}
+        // Create a graph
         vector<vector<pair<int, int>>> graph(n);
         for(auto f:flights){
             graph[f[0]].push_back({f[1], f[2]});
-        }
-        // Create a queue ds with the following pairs - {stops, {price, node}}
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}});
-        // Create a dist vector
+        } 
+        // dist vector - track price
         vector<int> dist(n, INT_MAX);
         dist[src] = 0;
-        // Perform BFS
+        // Create a queue - {stops, {node, price}}
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0, {src, 0}});
+        // Perform the bfs
         while(!q.empty()){
             auto val = q.front();
             q.pop();
-            int stops = val.first;
+            int stop = val.first;
             int city = val.second.first;
             int price = val.second.second;
-            // Check if the stops are less k+1
-            if(stops>k+1)continue;
-            for(auto next:graph[city]){
-                int nextcity = next.first;
-                int p = next.second;
-                if(stops+1<=k+1&&dist[nextcity]>p+price){
-                    dist[nextcity] = p+price;
-                    q.push({stops+1, {nextcity,dist[nextcity]}});
+            if(stop>k+1)continue;
+            for(auto adj:graph[city]){
+                int adjcity = adj.first;
+                int wt = adj.second;
+                if(stop+1<=k+1&&dist[adjcity]>price+wt){
+                    dist[adjcity] = price+wt;
+                    q.push({stop+1,{adjcity, price+wt}});
                 }
             }
         }
