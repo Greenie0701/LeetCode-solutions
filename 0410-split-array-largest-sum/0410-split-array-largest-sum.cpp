c5c6534d179ba35ebf21c92a41ffff2eae0dd21a
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-04 15:04:50 +0530

    Time: 0 ms (100%), Space: 10.4 MB (57.1%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-    bool possible(vector<int>& nums, int maxsum, int k){
-        int count =1;
-        int sum = 0;
-        for(int i:nums){
-            if(sum+i<=maxsum){
-                sum+=i;
+
+    // Helper function to count the sub arrays with sum max sum asmid
+    int count(vector<int>& arr, int maxi){
+        int count=1;
+        int sum =0;
+        for(int n:arr){
+            // If the sum plus next number is within maxi 
+            if(sum+n<=maxi){
+                sum+=n;
             }
-            else if(count<k){
-                sum=i;
-                count++;
-            }
+            // Else reinitialise the sum with n and increase the count
             else{
-                return false;
+                count++;
+                sum = n;
             }
         }
-        return true;
+        return count;
     }
 
     int splitArray(vector<int>& nums, int k) {
+        // The lowest split I can make should the maxi element of the arry
+        /*
+        7, 2, 5, 10, 8
+        2
+        10 - 32
+        21
+        10 - 20
+        16 - 20
+        18
+        16 - 19
+        17
+
+        */
+
         int low = *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(), nums.end(), 0);
-        int ans = 0;
+        // Apply binary search to find the optimal val
+        int ans = -1;
         while(low<=high){
             int mid = low+(high-low)/2;
-
-            if(possible(nums,mid, k)){
+            if(count(nums, mid)<=k){
                 ans = mid;
                 high = mid-1;
             }
