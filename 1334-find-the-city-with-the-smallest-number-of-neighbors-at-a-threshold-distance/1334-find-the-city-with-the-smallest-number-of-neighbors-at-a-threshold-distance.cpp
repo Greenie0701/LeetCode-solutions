a8b7a0ce240b4bd27847f659a246425e51cf219b
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-06 22:46:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,49 +1,39 @@
 class Solution {
 public:
-
-    vector<int> dijisktra(int n, vector<vector<pair<int, int>>>& graph, int src){
-        // Create a distance vector
-        vector<int> dist(n, INT_MAX);
-        dist[src]=0;
-        // Perform the dijistra 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, src});
-        while(!pq.empty()){
-            auto val = pq.top();
-            pq.pop();
-            int d = val.first;
-            int node = val.second;
-            // Iterate through the adj of nodes
-            for(auto adj:graph[node]){
-                int adjnode = adj.first;
-                int wt = adj.second;
-                if(dist[adjnode]>d+wt){
-                    dist[adjnode]=d+wt;
-                    pq.push({dist[adjnode], adjnode});
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        int mincity = INT_MAX;
+        int city = -1;
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        // initial configs
+        for(auto e:edges){
+            dist[e[0]][e[1]]=e[2];
+            dist[e[1]][e[0]]=e[2];
+        }
+        // Diagnol dist as 0
+        for(int i=0; i<n; i++){
+            dist[i][i]=0;
+        }
+        // Perform the Floyd
+        for(int k=0; k<n; k++){
+            for(int i=0; i<n; i++){
+                for(int j=0; j<n; j++){
+                    if(dist[i][k]!=INT_MAX&&dist[k][j]!=INT_MAX){
+                        dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+                    }
                 }
             }
         }
-        return dist;
-    }
-
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        int city = -1;
-        int citycnt = INT_MAX;
-        vector<vector<pair<int, int>>> graph(n);
-        for(auto e:edges){
-            graph[e[0]].push_back({e[1],e[2]});
-            graph[e[1]].push_back({e[0],e[2]});
-        }
+        // Count the cities less the the threshold and update the count
         for(int i=0; i<n; i++){
             int count = 0;
-            vector<int> dist = dijisktra(n, graph, i);
-            for (int j = 0; j < n; j++) {
-                if (j != i && dist[j] <= distanceThreshold) {
-                    count++;
-                }
+            for(int j=0; j<n; j++){
+                if(i!=j&&dist[i][j]<=distanceThreshold)count++;
             }
-            if(count<=citycnt){
-                citycnt=count;
+            if(count>0&&count<mincity){
+                city = i;
+                mincity = count;
+            }
+            else if(count==mincity){
                 city = i;
             }
         }
