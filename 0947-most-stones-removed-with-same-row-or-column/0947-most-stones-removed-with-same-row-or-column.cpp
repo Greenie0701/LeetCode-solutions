9b6254313ab763358304f418bbdaa24383f88f1e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-06 19:21:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -45,7 +45,7 @@ public:
         }
         disjointset ds(maxr+maxc+2);
         int offset = maxr+1;
-        unordered_set<int> pos;
+        unordered_map<int> pos;
         for(auto s:stones){
             ds.unionbyrank(s[0], s[1]+offset);
             pos.insert(s[0]);
