29a1767367e018abd9945473fb3c9a33cda48815
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-08 19:59:54 +0530

    Time: 95 ms (25.14%), Space: 48 MB (18.67%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Graph for the network
         vector<vector<pair<int, int>>> graph(n+1);
         for(auto t:times){
             graph[t[0]].push_back({t[1], t[2]});
@@ -15,17 +14,18 @@ public:
             pq.pop();
             int node = val.second;
             int time = val.first;
-            // Reach its neighbours
+            if(dist[node]<time)continue;
             for(auto adj:graph[node]){
                 int adjnode = adj.first;
                 int wt = adj.second;
                 if(dist[adjnode]>time+wt){
-                    dist[adjnode] = time+wt;
+                    dist[adjnode]=time+wt;
                     pq.push({dist[adjnode], adjnode});
                 }
             }
         }
-        int maxi_time = *max_element(dist.begin()+1, dist.end());
-        return maxi_time==INT_MAX?-1:maxi_time;
+        int maxi = *max_element(dist.begin()+1, dist.end());
+        if(maxi==INT_MAX)return -1;
+        return maxi;
     }
 };
