9627daeae4f5f093201c3e7a1080cb353c502744
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-08 20:10:18 +0530

    Time: 22 ms (7.81%), Space: 37.3 MB (41.61%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
+        vector<long long> dist(n, LLONG_MAX);
+        vector<long long> ways(n, 0);
         int mod = 1e9+7;
-        // Create a graph
+        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+        pq.push({0, 0});
+        ways[0]=1;
+        dist[0]=1;
         vector<vector<pair<int, int>>> graph(n);
         for(auto r:roads){
             graph[r[0]].push_back({r[1], r[2]});
             graph[r[1]].push_back({r[0], r[2]});
         }
-        vector<long long> dist(n, LLONG_MAX);
-        vector<long long> ways(n, 0);
-        dist[0] = 0;
-        ways[0] = 1;
-        set<pair<long long, int>> st;
-        st.insert({0, 0});
-        while(!st.empty()){
-            auto val = *st.begin();
-            st.erase(st.begin());
-            long long d = val.first;
+        while(!pq.empty()){
+            auto val = pq.top();
+            pq.pop();
             int node = val.second;
+            long long d = val.first;
             for(auto adj:graph[node]){
-                int adjnode = adj.first;
-                long long wt = adj.second;
+                int adjnode = adj.first; 
+                int wt = adj.second;
                 if(dist[adjnode]>d+wt){
-                    dist[adjnode]= d+wt;
-                    st.insert({dist[adjnode], adjnode});
-                    ways[adjnode] = ways[node];
+                    dist[adjnode]=d+wt;
+                    ways[adjnode]=ways[node];
+                    pq.push({dist[adjnode], adjnode});
                 }
                 else if(dist[adjnode]==d+wt){
-                    ways[adjnode]=((ways[adjnode]+ways[node])%mod);
+                    ways[adjnode]= (ways[adjnode]+ways[node])%mod;
                 }
             }
         }
-        return int(ways[n-1]);
+        return ways[n-1];
     }
 };
