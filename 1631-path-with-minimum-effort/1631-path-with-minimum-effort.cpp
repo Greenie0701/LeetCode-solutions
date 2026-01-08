9c56b549339874dee61b45943228f4800a6f5dcd
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-08 19:04:59 +0530

    Time: 39 ms (92.3%), Space: 24.3 MB (91.09%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -3,34 +3,27 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         // Priority queue
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        // Push the initial src
-        pq.push({0,{0,0}});
-        // Create a dist vector
+        pq.push({0, {0, 0}});
+        // Dist vector
         int m = heights.size();
         int n = heights[0].size();
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         dist[0][0]=0;
-        // Create a 4 directions vector
+        // Create a directional vector
         vector<int> drow = {1, -1, 0, 0};
         vector<int> dcol = {0, 0, 1, -1};
-        // Perform dijisktra
         while(!pq.empty()){
-            auto val = pq.top();
+            auto[effect, pos] = pq.top();
             pq.pop();
-            int effect = val.first;
-            int row = val.second.first;
-            int col = val.second.second;
-            // Check we have reached the end
+            int row = pos.first;
+            int col = pos.second;
             if(row==m-1&&col==n-1)return effect;
-            // Search in four directions
             for(int i=0; i<4; i++){
                 int r = row+drow[i];
                 int c = col+dcol[i];
-                // Check if they are valid pos
                 if(r>=0&&c>=0&&r<m&&c<n){
-                    int neweffect = max(effect, abs(heights[row][col]-heights[r][c]));
-                    if(neweffect<dist[r][c]){
-                        // Update the new effect for the pos
+                    int neweffect = max(effect, abs(heights[r][c]-heights[row][col]));
+                    if(dist[r][c]>neweffect){
                         dist[r][c]=neweffect;
                         pq.push({neweffect,{r,c}});
                     }
