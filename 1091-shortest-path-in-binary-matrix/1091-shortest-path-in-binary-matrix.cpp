f7fb85eb0f099aae007dd3780b005a9f60c38f49
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-08 18:51:51 +0530

    Time: 10 ms (90.25%), Space: 24.3 MB (69.5%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
+    bool isvalid(int r, int c, int row, int col){
+        return(r>=0&&c>=0&&r<row&&c<col);
+    }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        // Edge case what if start pos {0, 0} or end pos {n, n} contains 1
-        int size = grid.size();
-        if(grid[0][0]||grid[size-1][size-1])return -1;
-        // Create a visited array 
-        vector<vector<int>> visited(size, vector<int>(size, 0));
-        visited[0][0]=1;
-        // Create queue for bfs
+        int m = grid.size();
+        int n = grid[0].size();
+        if(grid[0][0]||grid[m-1][n-1])return -1;
         queue<pair<int, pair<int, int>>> q;
         q.push({1, {0, 0}});
-        // perform search in 8 directions
+        grid[0][0]=1;
         while(!q.empty()){
             auto val = q.front();
-            int len = val.first;
-            int row = val.second.first;
-            int col = val.second.second;
             q.pop();
-            if(row==size-1&&col==size-1)return len;
+            int step = val.first;
+            int row  = val.second.first;
+            int col  = val.second.second;
+            if(row==m-1&&col==n-1)return step;
             for(int i=-1; i<2; i++){
                 for(int j=-1; j<2; j++){
                     int r = row+i;
                     int c = col+j;
-                    if(r>=0&&c>=0&&r<size&&c<size){
-                        if(!grid[r][c]&&!visited[r][c]){
-                            visited[r][c]=1;
-                            q.push({len+1, {r, c}});
+                    if(isvalid(r, c, m, n)){
+                        if(!grid[r][c]){
+                            q.push({step+1, {r, c}});
+                            grid[r][c]=1;
                         }
                     }
                 }
