dc8b05c31396af906bc1bf51c9f11f4b0a38fcc4
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-08 19:24:19 +0530

    Time: 3 ms (66.29%), Space: 18.4 MB (42.9%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // Create a graph
+        vector<int> dist(n, INT_MAX);
+        dist[src]=0;
+        queue<pair<int, pair<int, int>>> q;
+        q.push({0,{src, 0}});
         vector<vector<pair<int, int>>> graph(n);
         for(auto f:flights){
             graph[f[0]].push_back({f[1], f[2]});
-        } 
-        // dist vector - track price
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-        // Create a queue - {stops, {node, price}}
-        queue<pair<int, pair<int, int>>> q;
-        q.push({0, {src, 0}});
-        // Perform the bfs
+        }
         while(!q.empty()){
             auto val = q.front();
             q.pop();
@@ -21,11 +17,11 @@ public:
             int price = val.second.second;
             if(stop>k+1)continue;
             for(auto adj:graph[city]){
-                int adjcity = adj.first;
+                int adjcity=adj.first;
                 int wt = adj.second;
                 if(stop+1<=k+1&&dist[adjcity]>price+wt){
-                    dist[adjcity] = price+wt;
-                    q.push({stop+1,{adjcity, price+wt}});
+                    dist[adjcity]=price+wt;
+                    q.push({stop+1,{adjcity,dist[adjcity]}});
                 }
             }
         }
