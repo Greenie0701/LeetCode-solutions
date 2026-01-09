5ecb1ed391485d44f75c8909529f675fb2952dd3
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-09 22:10:35 +0530

    Time: 17 ms (19.89%), Space: 17.2 MB (97.18%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,17 +10,15 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL||p->val==root->val||q->val==root->val){
-            return root;
-        }
+        if(!root||p->val==root->val||q->val==root->val)return root;
 
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
-        if(left==NULL){
+        
+        if(!left){
             return right;
         }
-        else if(right==NULL){
+        else if(!right){
             return left;
         }
         else{
