50695fd057ca676bf82ce0f1aaf05ce0867bd3b1
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-01-17 21:44:05 +0530

    Time: 291 ms (38.23%), Space: 159.5 MB (39.77%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -1,126 +1,99 @@
 class disjointset{
     public:
-    // Create parent and rank vector
-    vector<int> parent;
-    vector<int> rank;
-    vector<int> size;
-    // Define the disjointset constructor
+    vector<int> parent, size;
     disjointset(int n){
-        rank.resize(n, 0);
-        size.resize(n, 1);
         parent.resize(n);
-        // Intially keep parent of node itself
+        size.resize(n, 1);
         for(int i=0; i<n; i++){
             parent[i]=i;
         }
     }
-    // Find parent
-    int findparent(int u){
-        // BaseCase when parent of node itself
-        if(parent[u]==u){
-            return u;
-        }
-        //else backtrack 
-        return parent[u]=findparent(parent[u]);
+    // FInd the parent 
+    int findparent(int node){
+        if(parent[node]==node)return node;
+        return parent[node]=findparent(parent[node]);
     }
-    // Union by rank
-    void unionbyrank(int u, int v){
-        // Get the parents
-        int pu = findparent(u);
-        int pv = findparent(v);
-        // Same parent which means same set
-        if(pu==pv)return;
-        // Same rank so join with any one of the parent and increase rank
-        if(rank[pu]==rank[pv]){
-            parent[pv]=pu;
-            rank[pu]++;
-        } 
-        // Always attack smaller guy with larger rank guy
-        else if(rank[pu]<rank[pv]){
-            parent[pu]=pv;
-        }
-        else{
-            parent[pv]=pu;
-        }
+    // Find the size of node
+    int findsize(int node){
+        int p = findparent(node);
+        return size[p];
     }
-    int findsize(int u){
-        int pu = findparent(u);
-        return size[pu];
-    }
+    // Union by size
     void unionbysize(int u, int v){
-        // Get the parents
         int pu = findparent(u);
         int pv = findparent(v);
-        // Same parent which means same set
         if(pu==pv)return;
-        // Same size so join with any one of the parent and increase size
-        if(size[pu]==size[pv]){
+        int su = findsize(u);
+        int sv = findsize(v);
+        if(su==sv){
             parent[pv]=pu;
-            size[pu]+=size[pv];
-        } 
-        // Always attack smaller guy with larger size guy
-        else if(size[pu]<size[pv]){
+            size[pu]+=sv;
+        }
+        else if(su<sv){
             parent[pu]=pv;
-            size[pv]+=size[pu];
+            size[pv]+=su;
         }
         else{
             parent[pv]=pu;
-            size[pu]+=size[pv];
+            size[pu]+=sv;
         }
     }
 };
 
 class Solution {
 public:
+    bool isvalid(int row, int col, int m, int n){
+        return(row<m&&col<n&&row>=0&&col>=0);
+    }
     int largestIsland(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        disjointset ds(m*n);
         vector<int> drow={1, -1, 0, 0};
         vector<int> dcol={0, 0, 1, -1};
-        // Make the components of Island
+        disjointset ds(m*n);
         for(int row=0; row<m; row++){
             for(int col=0; col<n; col++){
-                if(!grid[row][col])continue;
-                for(int i=0; i<4; i++){
-                    int r=row+drow[i];
-                    int c=col+dcol[i];
-                    if(r>=0&&c>=0&&r<m&&c<n){
-                        int node = row*n+col;
-                        int adjnode = r*n+c;
-                        if(grid[r][c]){
-                            ds.unionbysize(node, adjnode);
+                if(grid[row][col]){
+                    for(int i=0; i<4; i++){
+                        int r = drow[i]+row; 
+                        int c = dcol[i]+col;
+                        if(isvalid(r, c, m, n)){
+                            int adjnode = r*n+c; 
+                            int node = row*n+col;
+                            if(grid[r][c]&&ds.findparent(node)!=ds.findparent(adjnode)){
+                                ds.unionbysize(node, adjnode);
+                            }
                         }
                     }
                 }
             }
         }
-        int maxi=0;
+        int max_area=0;
         for(int row=0; row<m; row++){
             for(int col=0; col<n; col++){
-                if(grid[row][col])continue;
-                unordered_set<int> components;
-                for(int i=0; i<4; i++){
-                    int r=row+drow[i];
-                    int c=col+dcol[i];
-                    if(r>=0&&c>=0&&r<m&&c<n){
-                        int adjnode = r*n+c;
-                        if(grid[r][c]){
-                            int parent = ds.findparent(adjnode);
-                            components.insert(parent);
+                unordered_set<int> s;
+                int sum = 0;
+                if(!grid[row][col]){
+                    for(int i=0; i<4; i++){
+                        int r = drow[i]+row; 
+                        int c = dcol[i]+col;
+                        if(isvalid(r, c, m, n)){
+                            int adjnode = r*n+c; 
+                            if(grid[r][c]){
+                                s.insert(ds.findparent(adjnode));
+                            }
                         }
                     }
                 }
-                int size=0;
-                for(auto it:components){
-                    size+=ds.findsize(it);
+                for(int it:s){
+                    sum+=ds.findsize(it);
                 }
-                maxi=max(maxi, size+1);
+                max_area=max(max_area, sum+1);
             }
         }
         for(int i=0; i<m*n; i++){
-            maxi = max(maxi, ds.findsize(ds.findparent(i)));
+            max_area = max(max_area, ds.findsize(ds.findparent(i)));
         }
-        return maxi;
+        return max_area;
     }
 };
