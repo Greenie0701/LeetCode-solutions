71010a9e6d98725c6cf2b10a61800af9626821d5
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-03 20:48:47 +0530

    Time: 33 ms (53.14%), Space: 44.3 MB (43.36%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -17,8 +17,27 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        int ans = dfs(n-1, coins, amount, dp);
-        return ans>=INF?-1:ans;
+        vector<int> prev(amount+1, INF);
+        // int ans = dfs(n-1, coins, amount, dp);
+        // return ans>=INF?-1:ans;
+        for (int t = 1; t <= amount; t++) {
+            if (t % coins[0] == 0)
+                prev[t] = t / coins[0];
+        }
+        prev[0]=0;
+        for(int i=1; i<n; i++){
+            vector<int> curr(amount+1, INF);
+            curr[0]=0;
+            for(int t=1; t<amount+1; t++){
+                int not_pick = prev[t];
+                int pick = INF;
+                if(coins[i]<=t){
+                    pick = 1+curr[t-coins[i]];
+                }
+                curr[t]=min(not_pick, pick);
+            }
+            prev = curr;
+        }
+        return prev[amount]==INF?-1:prev[amount];
     }
-};
+}; 
