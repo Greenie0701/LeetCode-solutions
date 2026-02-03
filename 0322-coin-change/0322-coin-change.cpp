2696c56725c267f6ec944503e857380c08d9eb85
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-03 19:57:16 +0530

    Time: 47 ms (33.43%), Space: 49.3 MB (28.73%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int INF = 1e9;
+    int dfs(int index, vector<int>& coins, int amount, vector<vector<int>>& dp){
+        if(amount==0)return 0;
+        if(index==0){
+            if(amount%(coins[0])==0)return(dp[index][amount]=amount/(coins[0]));
+            return INF;
+        }
+        if(index<0||amount<0)return INF;
+        if(dp[index][amount]!=-1)return dp[index][amount];
+        int not_pick = dfs(index-1, coins, amount, dp);
+        int pick = INF;
+        if(coins[index]<=amount){
+            pick = 1+dfs(index, coins, amount-coins[index], dp);
+        }
+        return dp[index][amount]=min(not_pick, pick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = dfs(n-1, coins, amount, dp);
+        return ans>=INF?-1:ans;
+    }
+};
