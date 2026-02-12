0e690c6cec654ecb3e0b9ba6c84587bab40f985f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-12 19:37:04 +0530

    Time: 3 ms (85.83%), Space: 79.2 MB (94.65%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
-    // int dfs(int i, int left, int canbuy, vector<int>& prices, int n, vector<vector<vector<int>>>& dp){
-    //     // Check if there is any transaction left/No more stocks left to buy
-    //     if(i==n||left==0)return 0;
-    //     if(dp[i][canbuy][left]!=-1)return dp[i][canbuy][left];
-    //     // Else
-    //     if(canbuy){
-    //         return dp[i][canbuy][left]=max(-prices[i]+dfs(i+1, left, 0, prices, n, dp),dfs(i+1, left, 1, prices, n, dp));
-    //     }
-    //     else{
-    //         return dp[i][canbuy][left]=max(prices[i]+dfs(i+1, left-1, 1, prices, n, dp), dfs(i+1, left, 0, prices, n, dp));
-    //     }
-    // }
     int maxProfit(vector<int>& prices) {
-        /*
-        The question is all about finding the maximum profit you can
-        by selling 2 transactions. Note than we can buy/sell 2 stocks
-        at a time. 1 transaction = buy & sell them.
-        so you can do buy1 -> sell1 ->buy2 -> sell2
-        You task is to find the maximum profit by making these 2 transactions
-        */
-        /* The dfs function explores all the possible way to either pick ith day stock
-           and sell/buy/skip the day based upon the following crieterias
-           1. left -> Check if we have transactions left
-           2. i -> Track the ith day in prices
-           3. prices
-           4. Can_buy -> Track whether you can buy the stock
-        */
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(3, vector<int>(2, 0)));
-        for(int i=n-1; i>=0; i--){
-            for(int left=1; left<=2; left++){
-                for(int canbuy=0; canbuy<=1; canbuy++){
-                    if(canbuy){
-                        dp[i][left][canbuy] = max(-prices[i]+dp[i+1][left][0], dp[i+1][left][1]);
-                    }
-                    else{
-                        dp[i][left][canbuy] = max(prices[i]+dp[i+1][left-1][1],dp[i+1][left][0]);
-                    }
-                }
-            }
+        int buy1 = INT_MIN, buy2 = INT_MIN;
+        int sell1 = 0, sell2 = 0;
+
+        for(int price : prices) {
+            buy1  = max(buy1, -price);
+            sell1 = max(sell1, buy1 + price);
+            buy2  = max(buy2, sell1 - price);
+            sell2 = max(sell2, buy2 + price);
         }
-        return dp[0][2][1];
-        // return dfs(0, 2, 1, prices, n, dp);
+
+        return sell2;
     }
 };
