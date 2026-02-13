712b70b5ed22efdc7e18f84370a29accc6ffda72
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-13 19:50:57 +0530

    Time: 175 ms (11.87%), Space: 22.7 MB (7.12%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
-public:
-    int jump(vector<int>& nums) {
-        int n=nums.size();
-        // Check if you have only one step
-        if(n==1)return 1;
-        if(n==1&&nums[0]==0)return 0;
-        // Steps tracks the minimum steps to reach final index
-        int steps =0;
-        // Start tracks the starting index in the range
-        int start=0;
-        // far tracks the end of the range
-        int far =0;
-        for(int i=0; i<n-1; i++){
-            far = max(far, i+nums[i]);
-            if(i==start){
-                start = far;
-                steps++;
-            }
+public: 
+    int dfs(int i, int dest, vector<int>& nums, vector<int>& dp){
+        if(i>=dest)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int steps = INT_MAX;
+        // Try out jumping all the possible from the current index to the max jump
+        for(int jump=1; jump<=nums[i]; jump++){
+            steps = min(steps, 1+dfs(i+jump, dest, nums, dp));
         }
-        return steps;
+        if(steps==INT_MAX)return dp[i]=1e9;
+        return dp[i]=steps;
+    }
+    int jump(vector<int>& nums) {
+        if(nums[0]==0)return 0;
+        int n = nums.size();
+        if(n==1)return 0;
+        vector<int> dp(n, -1);
+        return dfs(0, n-1, nums, dp);
     }
 };
