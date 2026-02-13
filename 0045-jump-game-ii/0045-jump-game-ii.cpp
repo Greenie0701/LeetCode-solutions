4fe2d98367361e2d977d1332f4e8782f64c9c118
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-13 19:11:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int start = 0;
-        int far = 0;
-        int step = 0;
-        for(int i=0; i<nums.size()-1; i++){
+        int n=nums.size();
+        // Check if you have only one step
+        if(n==1)return 1;
+        if(n==1&&nums[0]==0)return 0;
+        // Steps tracks the minimum steps to reach final index
+        int steps =0;
+        // Start tracks the starting index in the range
+        int start=0;
+        // far tracks the end of the range
+        int far =0;
+        for(int i=0; i<n-1; i++){
             far = max(far, i+nums[i]);
             if(i==start){
-                step++;
                 start = far;
+                steps++;
             }
         }
-        return step;
+        return steps;
     }
 };
