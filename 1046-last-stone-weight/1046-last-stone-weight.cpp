2cf929215ac80ead9efdf1817c8b75a02ff55317
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-16 20:44:34 +0530

    Time: 1 ms (7.06%), Space: 10 MB (47.95%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        if(stones.size()==1)return stones[0];
+        priority_queue<int> pq;
+        for(int s:stones){
+            pq.push(s);
+        }
+        while(pq.size()>1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            if(x!=y){
+                pq.push(y-x);
+            }
+        }
+        if(pq.empty())return 0;
+        return pq.top();
+    }
+};
