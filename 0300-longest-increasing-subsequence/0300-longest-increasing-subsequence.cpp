84d13d019d0f58afda35b0ab66008300669c7a7f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-16 18:44:42 +0530

    Time: 282 ms (31.81%), Space: 296.5 MB (12.81%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
 
-    int dfs(int i, int prev, vector<int>& nums, int n, vector<vector<int>>& dp){
-        if(i==n)return 0;
-        if(dp[i][prev+1]!=-1){
-            return dp[i][prev+1];
-        }
-        // Skip the index
-        int not_pick = dfs(i+1, prev, nums, n, dp);
-        // Try to pick the index
-        int pick = 0;
-        // Check if the conditions are made
-        if(prev==-1||nums[prev]<nums[i]){
-            pick=1+dfs(i+1, i, nums, n, dp);
-        }
-        // Return the max of you choice
-        return dp[i][prev+1]=max(pick, not_pick);
-    }
+    // int dfs(int i, int prev, vector<int>& nums, int n, vector<vector<int>>& dp){
+    //     if(i==n)return 0;
+    //     if(dp[i][prev+1]!=-1){
+    //         return dp[i][prev+1];
+    //     }
+    //     // Skip the index
+    //     int not_pick = dfs(i+1, prev, nums, n, dp);
+    //     // Try to pick the index
+    //     int pick = 0;
+    //     // Check if the conditions are made
+    //     if(prev==-1||nums[prev]<nums[i]){
+    //         pick=1+dfs(i+1, i, nums, n, dp);
+    //     }
+    //     // Return the max of you choice
+    //     return dp[i][prev+1]=max(pick, not_pick);
+    // }
 
     int lengthOfLIS(vector<int>& nums) {
         /*
@@ -28,8 +28,22 @@ public:
         2. You can skip the index and move to the next index
         */
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1, -1));
-        return dfs(0, -1, nums, n, dp);
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        // return dfs(0, -1, nums, n, dp);
+        for(int i=n-1; i>=0; i--){
+            for(int prev=i-1; prev>=-1; prev--){
+                int not_pick = dp[i+1][prev+1];
+                // Try to pick the index
+                int pick = 0;
+                // Check if the conditions are made
+                if(prev==-1||nums[prev]<nums[i]){
+                    pick=1+dp[i+1][i+1];
+                }
+                //the max of you choice
+                dp[i][prev+1]=max(pick, not_pick);
+            }
+        }
+        return dp[0][0];
     }
 };
 
