fe8fa9ed1a50e2c8720d1e24f69cc28de0f7e869
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-19 20:26:32 +0530

    Time: 0 ms (100%), Space: 23.6 MB (98.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // Find the first zero occuring element index in the array
-        int zeroindex = -1;
-        for(int j=0; j<nums.size()-1; j++){
-            if(nums[j]==0){
-                zeroindex=j;
+        int zero = -1;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                zero=i;
                 break;
             }
         }
-        // Base case when no zero in the array
-        if(zeroindex<0){
-            return;
-        }
-        //Swap the non-zero element with zeroth index
-        for(int j=zeroindex+1;j<nums.size();j++){
-            if(nums[j]!=0){
-                swap(nums[j],nums[zeroindex]);
-                zeroindex++;
+        if(zero==-1)return;
+        for(int i=zero+1; i<n; i++){
+            if(nums[i]!=0){
+                swap(nums[i], nums[zero]);
+                zero++;
             }
         }
-
     }
 };
