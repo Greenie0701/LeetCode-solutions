fcc4b43d4224cffaa350f527559cf2fac6a98fd8
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-20 19:26:58 +0530

    Time: 0 ms (100%), Space: 11.2 MB (98.43%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if(s.length()<t.length()){
-            return("");
-        }
-        int l = 0;
-        int r = 0;
-        int lens = s.length();
-        int lent = t.length();
-        int start = -1;
-        int mini = INT_MAX;
-        int count = 0;
+        int m = s.length();
+        int n = t.length();
         vector<int> hash(128, 0);
-        for(int i=0; i<lent; i++){
-            hash[t[i]]++;
+        int mini = INT_MAX;
+        int start = -1;
+        int count = 0;
+        for(char c:t){
+            hash[c]++;
         }
-        while(r<lens){
+        int l=0; int r=0;
+        while(r<m){
             if(hash[s[r]]>0)count++;
             hash[s[r]]--;
-            while(count==lent){
-                if((r-l+1)<mini){
-                    mini = (r-l+1);
+            while(count==n){
+                if(r-l+1<mini){
                     start = l;
+                    mini = r-l+1;
                 }
                 hash[s[l]]++;
                 if(hash[s[l]]>0)count--;
@@ -29,6 +25,6 @@ public:
             }
             r++;
         }
-        return start==-1?"":s.substr(start, mini);
+        return start<0?"":s.substr(start, mini);
     }
 };
