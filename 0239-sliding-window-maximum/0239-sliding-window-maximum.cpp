5c635b597606adae61312058187f8254ab354ba5
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-21 19:25:55 +0530

    Time: 21 ms (65.77%), Space: 139.3 MB (43.59%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
         deque<int> d;
-        vector<int> arr;
         for(int i=0; i<nums.size(); i++){
-            if(!d.empty()&&d.front()==i-k){
-                d.pop_front();
-            }
-            while(!d.empty()&&nums[d.back()]<nums[i]){
-                d.pop_back();
-            }
+            // Checks the maximum guy is part of current window
+            if(d.front()==i-k)d.pop_front();
+            // Maintain the dequeue in decreasing order
+            while(!d.empty()&&nums[d.back()]<=nums[i])d.pop_back();
+            // Push the current element in the dequeue
             d.push_back(i);
-            if(i>=k-1)arr.push_back(nums[d.front()]);
+            // Extract maximum of every k size window
+            if(i>=k-1)result.push_back(nums[d.front()]);
         }
-        return arr;
+        return result;
     }
 };
