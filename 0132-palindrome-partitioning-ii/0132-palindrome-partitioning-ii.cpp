a5030a882a1fb0fd67950e38ecdcc9c106aae42e
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-28 12:35:11 +0530

    Time: 1767 ms (11.84%), Space: 9.4 MB (75.22%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool ispalin(int i, int j, string& s){
+        while(i<j){
+            if(s[i]!=s[j])return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    int dfs(int i, int n, string& s, vector<int>& dp){
+        if(i==n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int cuts = 1e9;
+        // Try paritioning the string at k and check if the cut makes a palindrome cut
+        for(int k=i; k<n; k++){
+            if(ispalin(i, k , s)){
+                cuts = min(cuts, 1+dfs(k+1, n, s,dp));
+            }
+        }
+        return dp[i]=cuts;
+    }
+
+    int minCut(string s) {
+        int n = s.length();
+        if(n<=1)return 0;
+        string rev = s;
+        vector<int> dp(n, -1);
+        reverse(rev.begin(), rev.end());
+        if(rev==s)return 0;
+        return dfs(0, n, s, dp)-1;
+    }
+};
