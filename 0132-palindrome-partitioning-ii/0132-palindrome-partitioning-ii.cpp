16f99981d2c81c22ac2957df4fc7a953e34fe8eb
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-28 12:41:10 +0530

    Time: 1258 ms (51%), Space: 9.2 MB (86.37%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,26 +8,38 @@ public:
         }
         return true;
     }
-    int dfs(int i, int n, string& s, vector<int>& dp){
-        if(i==n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int cuts = 1e9;
-        // Try paritioning the string at k and check if the cut makes a palindrome cut
-        for(int k=i; k<n; k++){
-            if(ispalin(i, k , s)){
-                cuts = min(cuts, 1+dfs(k+1, n, s,dp));
-            }
-        }
-        return dp[i]=cuts;
-    }
+    // int dfs(int i, int n, string& s, vector<int>& dp){
+    //     if(i==n)return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     int cuts = 1e9;
+    //     // Try paritioning the string at k and check if the cut makes a palindrome cut
+    //     for(int k=i; k<n; k++){
+    //         if(ispalin(i, k , s)){
+    //             cuts = min(cuts, 1+dfs(k+1, n, s,dp));
+    //         }
+    //     }
+    //     return dp[i]=cuts;
+    // }
 
     int minCut(string s) {
         int n = s.length();
         if(n<=1)return 0;
         string rev = s;
-        vector<int> dp(n, -1);
+        vector<int> dp(n+1, 0);
+        dp[n]=0;
         reverse(rev.begin(), rev.end());
         if(rev==s)return 0;
-        return dfs(0, n, s, dp)-1;
+        // return dfs(0, n, s, dp)-1;
+        for(int i=n-1; i>=0; i--){
+            int cuts = 1e9;
+            // Try paritioning the string at k and check if the cut makes a palindrome cut
+            for(int k=i; k<n; k++){
+                if(ispalin(i, k , s)){
+                    cuts = min(cuts, 1+dp[k+1]);
+                }
+            }
+            dp[i]=cuts;
+        }
+        return dp[0]-1;
     }
 };
