bcf8add62adb098e2c203b7015ae7b84bbbdd577
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-02-28 19:05:31 +0530

    Time: 6 ms (56.41%), Space: 12.4 MB (59.93%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
 
-    int dfs(int i, int k, int n, vector<int>& arr, vector<int>& dp){
-        // Base case when you reach end of the array
-        if(i==n)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int sum = 0;
-        int maxi = INT_MIN;
-        // Try partitioning from the front till k 
-        int length = 0;
-        for(int j=i; j<min(n, i+k); j++){
-            length++;
-            maxi = max(maxi, arr[j]);
-            int remain = dfs(j+1, k, n, arr, dp);
-            sum = max(sum, maxi*length+remain);
-        }
-        return dp[i]=sum;
-    }
+    // int dfs(int i, int k, int n, vector<int>& arr, vector<int>& dp){
+    //     // Base case when you reach end of the array
+    //     if(i==n)return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     int sum = 0;
+    //     int maxi = INT_MIN;
+    //     // Try partitioning from the front till k 
+    //     int length = 0;
+    //     for(int j=i; j<min(n, i+k); j++){
+    //         length++;
+    //         maxi = max(maxi, arr[j]);
+    //         int remain = dfs(j+1, k, n, arr, dp);
+    //         sum = max(sum, maxi*length+remain);
+    //     }
+    //     return dp[i]=sum;
+    // }
 
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n , -1);
-        return dfs(0, k, n, arr, dp);
+        vector<int> dp(n+1, 0);
+        for(int i=n-1; i>=0; i--){
+            int sum = 0;
+            int maxi = INT_MIN;
+            // Try partitioning from the front till k 
+            int length = 0;
+            for(int j=i; j<min(n, i+k); j++){
+                length++;
+                maxi = max(maxi, arr[j]);
+                int remain = dp[j+1];
+                sum = max(sum, maxi*length+remain);
+            }
+            dp[i]=sum;
+        }
+        return dp[0];
+        // return dfs(0, k, n, arr, dp);
     }
 };
