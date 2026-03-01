2134272194978893fd328477683cdfa3dd32a98f
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-01 17:02:04 +0530

    Time: 107 ms (40.23%), Space: 103.3 MB (19.57%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
-    // int dfs(int i, int canbuy, vector<int>& prices, int fee, vector<vector<int>>& dp){
-    //     if(i==prices.size())return 0;
-    //     if(dp[i][canbuy]!=-1)return dp[i][canbuy];
-    //     // Check if you can buy a stock
+    
+    // int dfs(int i, int canbuy, vector<int>& prices, int fee, int& n){
+    //     if(i==n)return 0;
+    //     // Every day you can either buy a stock or sell stock based on the canbuy
     //     if(canbuy){
-    //         /*
-    //         If you can buy a stock, you have two options:
-    //         1. Buy a stock and sell in future
-    //         2. Skip buying it today and buy it in future
-    //         Return max out of these two options
-    //         */
-    //         return dp[i][canbuy]=max(-prices[i]+dfs(i+1, 0, prices, fee, dp), dfs(i+1, 1, prices, fee, dp));
+    //         return max(-prices[i]+dfs(i+1, 0, prices, fee, n), dfs(i+1, 1, prices, fee, n));
     //     }
     //     else{
-    //         /*
-    //         If you can sell a stock, you have two options:
-    //         1. Sell a stock and buy in future with fee
-    //         2. Skip selling it today and sell it in future
-    //         Return max out of these two options
-    //         */
-    //         return dp[i][canbuy]=max(prices[i]-fee+dfs(i+1,1,prices,fee, dp), dfs(i+1, 0, prices, fee, dp));
+    //         return max(prices[i]-fee+dfs(i+1, 1, prices, fee, n), dfs(i+1, 0, prices, fee, n));
     //     }
     // }
+
     int maxProfit(vector<int>& prices, int fee) {
-        /*
-        You can do any transaction
-        but with the following conditions:
-        1. You can hold at most 1 stock
-        2. But one condition for every transaction you must pay fee
-        */
         int n = prices.size();
-        int aheadbuy =0;
-        int aheadsell=0;
+        // return dfs(0, 1, prices, fee, n);
         vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        // return dfs(0, 1, prices, fee, dp);
         for(int i=n-1; i>=0; i--){
-            int currbuy = max(-prices[i]+aheadsell, aheadbuy);
-            int currsell = max(prices[i]-fee+aheadbuy, aheadsell);
-            aheadbuy = currbuy;
-            aheadsell = currsell;
+            dp[i][1]=max(-prices[i]+dp[i+1][0], dp[i+1][1]);
+            dp[i][0]=max(prices[i]-fee+dp[i+1][1],dp[i+1][0]);
         }
-        return aheadbuy;
+        return dp[0][1];
     }
 };
