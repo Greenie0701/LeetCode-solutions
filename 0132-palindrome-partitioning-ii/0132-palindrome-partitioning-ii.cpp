fe708c13f663d881036775fd9ee029c102dc0ee7
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-01 17:41:22 +0530

    Time: 1261 ms (50.38%), Space: 9.2 MB (86.43%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,40 +1,41 @@
 class Solution {
 public:
-    bool ispalin(int i, int j, string& s){
+
+    bool isPalin(int i, int j, string& s){
         while(i<j){
             if(s[i]!=s[j])return false;
             i++;
             j--;
         }
+        
         return true;
     }
-    // int dfs(int i, int n, string& s, vector<int>& dp){
+
+    // int dfs(int i, int n, string& s){
     //     if(i==n)return 0;
-    //     if(dp[i]!=-1)return dp[i];
-    //     int cuts = 1e9;
-    //     // Try paritioning the string at k and check if the cut makes a palindrome cut
+    //     int cuts = INT_MAX;
+    //     // Lets try to parition at every possible k and return the minimal cut
     //     for(int k=i; k<n; k++){
-    //         if(ispalin(i, k , s)){
-    //             cuts = min(cuts, 1+dfs(k+1, n, s,dp));
+    //         if(isPalin(i, k, s)){
+    //             cuts = min(cuts, 1+dfs(k+1, n, s));
     //         }
     //     }
-    //     return dp[i]=cuts;
+    //     return cuts;
     // }
 
     int minCut(string s) {
         int n = s.length();
         if(n<=1)return 0;
         string rev = s;
-        vector<int> dp(n+1, 0);
-        dp[n]=0;
         reverse(rev.begin(), rev.end());
         if(rev==s)return 0;
-        // return dfs(0, n, s, dp)-1;
+        // return dfs(0, n, s)-1;
+        vector<int> dp(n+1, 0);
         for(int i=n-1; i>=0; i--){
-            int cuts = 1e9;
-            // Try paritioning the string at k and check if the cut makes a palindrome cut
+            int cuts = INT_MAX;
+            // Lets try to parition at every possible k and return the minimal cut
             for(int k=i; k<n; k++){
-                if(ispalin(i, k , s)){
+                if(isPalin(i, k, s)){
                     cuts = min(cuts, 1+dp[k+1]);
                 }
             }
