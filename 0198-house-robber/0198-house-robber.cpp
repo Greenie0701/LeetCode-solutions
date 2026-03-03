75822f378efc27dc3bd954ff4e28bed29c6cf598
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-03 18:52:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    // int dfs(int index, vector<int>& nums, vector<int>& dp){
-    //     if(index==0)return dp[0]=nums[0];
-    //     if(index<0)return 0;
-    //     if(dp[index]!=-1)return dp[index];
-    //     int not_pick = dfs(index-1, nums, dp);
-    //     int pick = nums[index]+dfs(index-2, nums, dp);
-    //     return dp[index]=max(pick, not_pick);
+
+    // int dfs(int i, vector<int>& nums, vector<int>& dp){
+    //     if(i<0)return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     if(i==0)return dp[i]=nums[0]; 
+    //     int not_pick = dfs(i-1, nums, dp);
+    //     int pick = nums[i]+dfs(i-2, nums, dp);
+    //     return max(pick, not_pick);
     // }
+
+
     int rob(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
         int n = nums.size();
+        if(n==1)return nums[0];
         vector<int> dp(n, 0);
+        // return dfs(n-1, nums, dp);
         dp[0]=nums[0];
         dp[1]=max(nums[0], nums[1]);
-        for(int i=1; i<n; i++){
+        for(int i=2; i<n; i++){
             int not_pick = dp[i-1];
             int pick = 0;
             if(i>1){
                 pick = nums[i]+dp[i-2];
             }
-            dp[i]=max(not_pick, pick);
+            dp[i]=max(pick, not_pick);
         }
         return dp[n-1];
-        // return dfs(n-1, nums, dp);
     }
 };
-
-// 1 2 3 1
-//     3 1 = 4
-//     2 1 = 3
