d7a5541bfe1bd38cf1618ab19aeb757b812f61de
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-04 20:50:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-
-    // int dfs(int n, vector<int>& dp){
-    //     if(n==0)return 0;
-    //     if(dp[n]!=-1)return dp[n];
-    //     int ans = 1e9;
-    //     for(int i=1; i*i<=n; i++){
-    //         ans = min(ans, 1+dfs(n-(i*i), dp));
-    //     }
-    //     return dp[n]=ans;
-    // }
+    int dfs(int n){
+        if(n<=0)return 0;
+        int ans = 1e9;
+        for(int i=1; i*i<=n; i++){
+            ans = min(ans, 1+dfs(n-(i*i)));
+        }
+        return ans;
+    }
 
     int numSquares(int n) {
+        // return dfs(n);
         vector<int> dp(n+1, 1e9);
         dp[0]=0;
-        // return dfs(n, dp);
         for(int i=1; i<=n; i++){
             for(int j=1; j*j<=i; j++){
-                dp[i] = min(dp[i], 1+dp[(i-(j*j))]);
+                dp[i]  = min(dp[i], 1+dp[i-(j*j)]);
             }
         }
         return dp[n];
     }
 };
-// 5
-
-// dp[0 , 1 , 2, 3, 1, 2]
