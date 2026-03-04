e077618f9f032c7cb013729b5dcaa838c504a73c
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-04 15:57:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (53.03%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
+
+    int dfs(int i, string& s){
+        if(i<=0)return 1;
+        int one_pick = s[i]!='0'?dfs(i-1, s):0;
+        int val = ((s[i-1]-'0')*10)+(s[i]-'0');
+        int two_pick = val>=10&&val<=26?dfs(i-2, s):0;
+        return two_pick+one_pick;
+    }
+
     int numDecodings(string s) {
-        if (s[0] == '0') return 0;
-
+        // Base case when first index contains '0'
+        if(s[0]=='0')return 0;
         int n = s.length();
-        if (n < 2) return 1;
-
         vector<int> dp(n, 0);
+        if(n==1)return 1;
         dp[0] = 1;
 
-        int val = (s[0]-'0')*10 + (s[1]-'0');
+        dp[1] = (s[1] != '0') ? dp[0] : 0;
 
-        dp[1] = 0;
-
-        // single digit contribution
-        if (s[1] != '0')
-            dp[1] += dp[0];
-
-        // two digit contribution
-        if (val >= 10 && val <= 26)
+        int val = ((s[0]-'0')*10)+(s[1]-'0');
+        if(val >= 10 && val <= 26) 
             dp[1] += 1;
-
-        for (int i = 2; i < n; i++) {
-
-            int one = (s[i] == '0') ? 0 : dp[i-1];
-
-            val = (s[i-1]-'0')*10 + (s[i]-'0');
-            int two = (val >= 10 && val <= 26) ? dp[i-2] : 0;
-
-            dp[i] = one + two;
+        for(int i=2; i<n; i++){
+            int one_pick = s[i]!='0'?dp[i-1]:0;
+            val = ((s[i-1]-'0')*10)+(s[i]-'0');
+            int two_pick = val>=10&&val<=26?dp[i-2]:0;
+            dp[i]=two_pick+one_pick;
         }
-
         return dp[n-1];
+        // return dfs(n-1, s);
     }
 };
