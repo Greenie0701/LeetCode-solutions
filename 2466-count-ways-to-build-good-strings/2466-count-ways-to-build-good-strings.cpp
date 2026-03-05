35d27a4c90e2e9ebd45b110d12ce79e62e025016
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-05 19:32:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int mod =1e9 + 7;
-    int dfs(int len, int low, int high, int zero, int one, vector<int>& dp){
-        if(len>high)return 0;
-        if(dp[len]!=-1)return dp[len];
-        long long ans = 0;
-        if(len>=low)ans++;
-        ans+=dfs(len+zero, low, high, zero, one, dp);
-        ans+=dfs(len+one, low, high, zero, one, dp);
+    // int dfs(int len, int low, int high, int zero, int one, vector<int>& dp){
+    //     if(len>high)return 0;
+    //     if(dp[len]!=-1)return dp[len];
+    //     long long ans = 0;
+    //     if(len>=low)ans++;
+    //     ans+=dfs(len+zero, low, high, zero, one, dp);
+    //     ans+=dfs(len+one, low, high, zero, one, dp);
 
-        return dp[len]=ans%mod;
-    }
+    //     return dp[len]=ans%mod;
+    // }
 
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high+1, -1);
-        return dfs(0, low, high, zero, one, dp);
+        // vector<int> dp(high+1, -1);
+        // return dfs(0, low, high, zero, one, dp);
+        int maxi = max(zero, one);
+        vector<int> dp(high+maxi+1, 0);
+        for(int len=high; len>=0; len--){
+            long long ans = 0;
+            if(len>=low&&len<=high)ans++;
+            ans+=dp[len+zero];
+            ans+=dp[len+one];
+
+            dp[len]=ans%mod;
+        }
+        return dp[0];
     }
 };
