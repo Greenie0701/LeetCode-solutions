d0249fdbb27480663da20069cf45c520588a7d35
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-09 20:35:52 +0530

    Time: 0 ms (100%), Space: 29.6 MB (43.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    void reverse(vector<int>& nums, int start, int end){
-        int temp=0;
+    void reverse(int start, int end, vector<int>& nums){
         while(start<end){
-            temp = nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
+            swap(nums[start], nums[end]);
             start++;
             end--;
         }
     }
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        // Base case when array size contains one element
-        if(n==0){
-            return;
-        }
-        // Find the  number of rotations which are required
-        k=k%n; 
-        /* To rotate array by k positions, you may have perform the following operations:
-           1. Reverse elements in array from 0 to n-k-1
-           2. Reverse elements in array from n-k to n-1
-           3. Reverse elements in array from 0 to n-1(complete array)
-        */ 
-        reverse(nums, 0, n-k-1);
-        reverse(nums, n-k, n-1);
-        reverse(nums, 0, n-1);
+        int size=nums.size();
+        k = k%size;
+        reverse(0,size-k-1,nums);
+        reverse(size-k, size-1, nums);
+        reverse(0,size-1,nums);
     }
 };
