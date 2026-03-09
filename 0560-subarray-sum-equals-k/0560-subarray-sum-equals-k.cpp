071aef26e74030e1a15af1daea404aaecb4d449c
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-09 20:48:32 +0530

    Time: 39 ms (72.39%), Space: 45.5 MB (39.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefix;
+        unordered_map<int, int> prefix_count;
         int count = 0;
-        int sum=0;
-        prefix[0]=1;
+        int sum = 0;
+        prefix_count[0]=1;
         for(int i=0; i<nums.size(); i++){
             sum+=nums[i];
-            if(prefix.find(sum-k)!=prefix.end()){
-                count+=prefix[sum-k];
+            if(prefix_count.find(sum-k)!=prefix_count.end()){
+                count+=prefix_count[sum-k];
             }
-            prefix[sum]++;
+            prefix_count[sum]++;
         }
         return count;
     }
 };
+
+// 1 2 3 3 4   6
+// 1 3 6 9 13
+// 1 1 1 1 1
+
+// 2
