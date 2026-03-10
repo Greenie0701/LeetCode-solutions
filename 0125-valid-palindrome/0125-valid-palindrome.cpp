df6d73340abe46c68e2af0697844fa0dca6faa83
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-10 17:56:00 +0530

    Time: 0 ms (100%), Space: 10.2 MB (23.98%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string result = "";
+        for(char c:s){
+            if(isalnum(c)){
+                result+=tolower(c);
+            }
+        }
+        if(result.empty())return true;
+        int i=0;
+        int j=result.size()-1;
+        while(i<j){
+            if(result[i]!=result[j])return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
