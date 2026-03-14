1a8681b8736a30896099de6d2ca4beeea924fb67
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-14 09:35:29 +0530

    Time: 51 ms (24.75%), Space: 49.4 MB (29.7%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    int INF = 1e9;
-    int dfs(int index, vector<int>& coins, int amount, vector<vector<int>>& dp){
-        if(index==0){
-            if(amount%(coins[0])==0)return(dp[index][amount]=amount/(coins[0]));
-            return INF;
+    int dfs(int i, vector<int>& coins, int amount, vector<vector<int>>& dp){
+        if(dp[i][amount]!=-1)return dp[i][amount];
+        if(amount==0)return dp[i][amount]=0;
+        if(i==0){
+            if(amount%coins[0]==0)return dp[i][amount]=amount/coins[0];
+            return 1e9;
         }
-        if(index<0||amount<0)return INF;
-        if(dp[index][amount]!=-1)return dp[index][amount];
-        int not_pick = dfs(index-1, coins, amount, dp);
-        int pick = INF;
-        if(coins[index]<=amount){
-            pick = 1+dfs(index, coins, amount-coins[index], dp);
+        int not_pick = dfs(i-1, coins, amount, dp);
+        int pick = 1e9;
+        if(coins[i]<=amount){
+            pick = 1+dfs(i, coins, amount-coins[i], dp);
         }
-        return dp[index][amount]=min(not_pick, pick);
+        return dp[i][amount]=min(pick, not_pick);
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<int> prev(amount+1, INF);
-        // int ans = dfs(n-1, coins, amount, dp);
-        // return ans>=INF?-1:ans;
-        for (int t = 1; t <= amount; t++) {
-            if (t % coins[0] == 0)
-                prev[t] = t / coins[0];
-        }
-        prev[0]=0;
-        for(int i=1; i<n; i++){
-            vector<int> curr(amount+1, INF);
-            curr[0]=0;
-            for(int t=1; t<amount+1; t++){
-                int not_pick = prev[t];
-                int pick = INF;
-                if(coins[i]<=t){
-                    pick = 1+curr[t-coins[i]];
-                }
-                curr[t]=min(not_pick, pick);
-            }
-            prev = curr;
-        }
-        return prev[amount]==INF?-1:prev[amount];
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int result = dfs(n-1, coins, amount, dp);
+        return result==1e9?-1:result;
     }
-}; 
+};
