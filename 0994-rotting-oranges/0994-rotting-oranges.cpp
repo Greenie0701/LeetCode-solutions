06215588e7e97cff5276d4a88bec01b2ff6ac40c
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-15 20:48:53 +0530

    Time: 0 ms (100%), Space: 16.7 MB (71.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
+    bool isvalid(int row, int col, int m, int n){
+        return(row>=0&&row<m&&col>=0&&col<n);
+    }
     int orangesRotting(vector<vector<int>>& grid) {
+        vector<int> drow = {1, -1, 0, 0};
+        vector<int> dcol = {0, 0, 1, -1};
+        queue<pair<int, pair<int, int>>> q;
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> rotten(m, vector<int>(n, 0));
-        queue<pair<pair<int, int>, int>> q;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==2){
-                    rotten[i][j]=2;
-                    q.push({{i, j},0});
+                    q.push({0, {i, j}});
                 }
             }
         }
         int time = 0;
-        vector<int> drow = {1, -1, 0, 0}; 
-        vector<int> dcol = {0, 0, 1, -1};
         while(!q.empty()){
-            int x = q.front().first.first;
-            int y = q.front().first.second;
-            int t = q.front().second;
+            auto val = q.front();
             q.pop();
+            int t = val.first;
+            int row = val.second.first;
+            int col = val.second.second;
             time = max(t, time);
             for(int i=0; i<4; i++){
-                int r = x+drow[i];
-                int c = y+dcol[i];
-                if(r>=0&&r<m&&c>=0&&c<n&&!rotten[r][c]&&grid[r][c]==1){
-                    q.push({{r, c}, t+1});
-                    rotten[r][c]=2;
+                int r = row+drow[i];
+                int c = col+dcol[i];
+                if(isvalid(r, c, m, n)){
+                    if(grid[r][c]==1){
+                        grid[r][c]=0;
+                        q.push({t+1, {r, c}});
+                    }
                 }
             }
         }
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(grid[i][j]==1&&rotten[i][j]!=2)return -1;
+                if(grid[i][j]==1){
+                    return -1;
+                }
             }
         }
         return time;
