5d569c164cef2656ae7246b4dba2bb8b634454c4
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-17 17:59:20 +0530

    Time: 1 ms (24.36%), Space: 21.6 MB (99.4%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int moves = 0;
+        int n = seats.size();
         sort(seats.begin(), seats.end());
         sort(students.begin(), students.end());
-        int moves = 0;
-        for(int i=0; i<seats.size(); i++){
-            moves+=(abs(seats[i]-students[i]));
+        for(int i=0; i<n; i++){
+            moves+=abs(seats[i]-students[i]);
         }
         return moves;
     }
