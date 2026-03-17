9d838048fe3d99e5e04f0cf0cda18d2d837a5a92
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-17 19:01:01 +0530

    Time: 37 ms (19.15%), Space: 73.1 MB (29.39%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if(n==1)return 1;
-        vector<int> I_trust(n+1, 0);
-        vector<int> others(n+1, 0);
+        // vector<int> I_trust(n+1, 0);
+        // vector<int> others(n+1, 0);
+        vector<int> scores(n+1, 0);;
         for(auto t:trust){
-            others[t[1]]++;
-            I_trust[t[0]]++;
+            scores[t[1]]++;
+            scores[t[0]]--;
         }
         for(int i=1; i<n+1; i++){
-            if(others[i]==n-1&&I_trust[i]==0)return i;
+            if(scores[i]==n-1)return i;
         }
         return -1;
     }
