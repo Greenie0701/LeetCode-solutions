380cfb511d457bb1a3af58fe752679ef7bf2b950
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-23 19:26:02 +0530

    Time: 74 ms (14.16%), Space: 90.1 MB (37.28%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums) {
-        int maxi = INT_MIN;
+
         sort(nums.rbegin(), nums.rend());
         long long prefix = 0;
         int score = 0;
@@ -11,11 +11,11 @@ public:
                 score++;
             }
             else{
-                maxi = max(maxi, score);
+                
                 break;
             }
-            maxi = max(maxi, score);
+
         }
-        return maxi;
+        return score;
     }
 };
