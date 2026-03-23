7dfb0935585795d0bee698235c542420a7711906
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-23 18:35:05 +0530

    Time: 4 ms (79.6%), Space: 13.2 MB (68.27%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int partitionString(string s) {
+        // Captures the char in the string
         int mask = 0;
+        // Captures individual chars set bit
         int bit = 0;
         int count =1;
+        // Go through every char in the string
         for(char& c:s){
+            // Set the cth bit
             bit = 1<<(c-'a');
+            // Now compare the bit with mask(captures the duplicate)
             if(mask&bit){
-                count++;
                 mask = 0;
+                count++;
             }
+            // Add char to the mask
             mask|=bit;
         }
         return count;
