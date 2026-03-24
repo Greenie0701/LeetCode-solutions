e6264fa51b813e69b98aad30d926186065b5be94
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-24 20:37:17 +0530

    Time: 39 ms (54.04%), Space: 28.1 MB (50.19%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
     string repeatLimitedString(string s, int repeatLimit) {
-        string result = "";
-        vector<int> freq(26, 0);
-        for(char& c:s){
-            freq[c-'a']++;
-        }
         priority_queue<pair<int, int>> pq;
-        for(int i=0; i<26; i++){
-            if(!freq[i])continue;
-            pq.push({i, freq[i]});
+        vector<int> freq(26, 0);
+        for(char& ch:s){
+            freq[ch-'a']++;
         }
+        for(int i=0; i<26; i++){
+            if(freq[i])pq.push({i, freq[i]});
+        }
+        string result="";
         while(!pq.empty()){
             auto val = pq.top();
             pq.pop();
-            if(!result.empty() && val.first+'a'==result.back()){
+            if(!result.empty()&&result.back()==(val.first+'a')){
                 if(pq.empty())break;
                 auto val2 = pq.top();
                 pq.pop();
                 result.append(1, val2.first+'a');
+                if(val2.second!=1)pq.push({val2.first, val2.second-1});
                 pq.push(val);
-                if(val2.second==1)continue;
-                pq.push({val2.first, val2.second-1});
             }
             else{
-                int f = min(val.second, repeatLimit);
-                result.append(f, (val.first+'a'));
-                if(repeatLimit<val.second)pq.push({val.first, val.second-repeatLimit});
+                result.append(min(repeatLimit, val.second), val.first+'a');
+                if(val.second>repeatLimit)pq.push({val.first, val.second-repeatLimit});
             }
         }
         return result;
     }
 };
-
-// a - 4
-// b - 3
-
-// 2
-// [1, 1]
-// [0, 4]
-
-// "bb"
-
-
