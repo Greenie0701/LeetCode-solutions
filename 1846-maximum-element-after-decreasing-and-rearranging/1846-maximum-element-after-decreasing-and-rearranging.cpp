078e61d25ead13ebfdaf4b1131937e575d5631ec
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-27 21:26:48 +0530

    Time: 7 ms (67.09%), Space: 55.1 MB (70.89%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        // Sort the array in increasing order
         sort(arr.begin(), arr.end());
-        // Check if the first element is 1, if not set to 1
         if(arr[0]!=1)arr[0]=1;
-        // Set maxi as 1;
-        int maxi = arr[0];
+        int maxi = 1;
         for(int i=1; i<arr.size(); i++){
             if(arr[i]-arr[i-1]>1){
                 arr[i]=arr[i-1]+1;
             }
-            maxi = max(arr[i], maxi);
+            maxi = max(maxi, arr[i]);
         }
         return maxi;
     }
