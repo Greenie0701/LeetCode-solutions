cb39a2db079be2fe6e6aeab890d7186e13292100
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-03-27 22:05:32 +0530

    Time: 193 ms (13.73%), Space: 117.4 MB (6.06%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -3,9 +3,13 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         long long sum = 0;
         int turn = 0;
-        sort(happiness.rbegin(), happiness.rend());
+        priority_queue<int> pq;
+        for(int& n:happiness){
+            pq.push(n);
+        }
         for(int i=0; i<k; i++){
-            sum+=max(0, happiness[i]-turn);
+            sum+=max(pq.top()-turn, 0);
+            pq.pop();
             turn++;
         }
         return sum;
