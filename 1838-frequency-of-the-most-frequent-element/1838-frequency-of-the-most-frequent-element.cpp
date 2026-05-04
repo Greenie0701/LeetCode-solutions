c1062ea51573e49cc87124fc94434163d2bd6e89
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-05-04 20:35:06 +0530

    Time: 39 ms (60.5%), Space: 105.8 MB (92.6%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
+        long long sum =0;
         int left = 0;
-        int right = 0;
-        long long total_sum=0;
-        int maxi = 1;
+        int ans = 1;
+        sort(nums.begin(), nums.end());
         for(int right=0; right<nums.size(); right++){
-            int target = nums[right];
-            total_sum+=nums[right];
-            // Check if it is not valid window
-            while(right<nums.size()&&(long long)(right-left+1)*(target)>total_sum+k){
-                total_sum-=nums[left];
+            sum+=nums[right];
+            int target=nums[right];
+            while(right<nums.size()&&((long long)(right-left+1)*(long long)target>sum+k)){
+                sum-=nums[left];
                 left++;
             }
-            maxi = max(maxi, right-left+1);
+            ans = max(ans, right-left+1);
         }
-        return maxi;
+        return ans;
     }
 };
