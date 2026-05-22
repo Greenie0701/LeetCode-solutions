11e95f41c7b704373c2a6714bc67e28ec304e7ae
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-05-22 20:04:35 +0530

    Time: 0 ms (100%), Space: 25 MB (64.3%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -12,21 +12,19 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        vector<vector<int>> bfs;
+        int result = 0;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
             int size = q.size();
-            vector<int> level;
             for(int i=0; i<size; i++){
                 TreeNode* node = q.front();
                 q.pop();
-                level.push_back(node->val);
+                if(i==0)result=node->val;
                 if(node->left)q.push(node->left);
                 if(node->right)q.push(node->right);
             }
-            bfs.push_back(level);
         }
-        return bfs.back()[0];
+        return result;
     }
 };
