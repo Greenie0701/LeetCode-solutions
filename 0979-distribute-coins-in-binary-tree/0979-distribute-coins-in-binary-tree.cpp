0af0b2ccc82b1b3d27b3d42ad8922c627c5653b9
MS-GITS <137760120+Greenie0701@users.noreply.github.com>
2026-06-01 20:46:39 +0530

    Time: 0 ms (100%), Space: 16.3 MB (98.84%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    int moves=0;
+    int moves = 0;
+
     int dfs(TreeNode* root){
         if(!root)return 0;
         int left = dfs(root->left);
@@ -20,6 +21,7 @@ public:
         moves+=abs(right);
         return root->val+left+right-1;
     }
+
     int distributeCoins(TreeNode* root) {
         dfs(root);
         return moves;
