fe9d4a57185c2a19dfe1a11bfbe7ef165d97a94e
Mugundan S <smugundan12a@gmail.com>
2026-08-22 20:50:38 +0530

    Time: 2 ms (15.15%), Space: 9.8 MB (9.44%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> result;
+        if(!numRows)return result;
+        // First row always zero
         result.push_back({1});
-        if(numRows==1){
-            return result;
-        }
+        // Start from second row & generate the remain n-1 rows
         for(int i=1; i<numRows; i++){
+            // Define a empty vec to hold each row result
             vector<int> row;
-            vector<int> prev = result.back();
+            // First value of row is always 1
             row.push_back(1);
-            for(int j=1; j<prev.size(); j++){
-                row.push_back(prev[j-1]+prev[j]);
+            // Start from sec value
+            for(int j=1; j<result.back().size(); j++){
+                row.push_back(result[i-1][j-1]+result[i-1][j]);
             }
             row.push_back(1);
             result.push_back(row);
